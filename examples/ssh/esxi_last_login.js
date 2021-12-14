9c3d717725959040f4813850ddfa5f5b5af06a4f
Georgi Tsatsev <gtsatsev@domotz.com>
2021-12-14 13:12:54 +0200

    SSB-1216: added ESXI Last Login detection Driver

--- examples/ssh/esxi_last_login.js
+++ examples/ssh/esxi_last_login.js
@@ -1,28 +1,14 @@
+/**
+ * This Driver Extracts the Last Login line for an ESXI device.
+ * Communication protocol is SSH.
+ * Creates a single custom driver variables with label "Last Login" containging the following information:
+ *   - The user name that logged in
+ *   - The IP Address from which the ssh session was initiated
+ */
+
 var lastLoginUID = 'll-1';
 var lastLoginLabel = 'Last Login';
-var lastLoginUnit = 'text';
-
-var COMMAND_VALIDATE = 'ls'
-/**
- *  'grep' matches expressions 
- * in the '/var/log/auth.log' file:
- *'login' for a login at the console;
- *'accepted' for a successful SSH login;
- * option '-i' [i]gnores case;
- * option '-e' gives the [e]xpression
- * that follows;
- * '|' pipes the output
- * as input for the next command; 
- * 'tail' takes the last lines of input;
- * option '-n2' takes the last 
- * '2' [n]umber of lines, as the 
- * last line will be your current login;
- * '|' pipes once again;
- * 'head' takes the first lines of input;
- * option '-n1' takes the first '1' line 
- * (the last login before your current one);
-*/
-var COMMAND_GET_STATUS = 'grep -i -e login -e accepted /var/log/auth.log | grep -v "grep" | tail -n2 | head -n1';
+var lastLoginUnit = 'line';
 
 /**
  * The SSH Command Options
@@ -36,9 +22,9 @@ var sshCommandOptions = {
  * verifies ssh command did not result in a password error
  * Calls D.failure in case the authentication with the device failed (error code 5)
 */
-function checkForPasswordError(error){
+function checkForPasswordError(error) {
     console.error("Received an error during execution", error);
-    if (error.code === 5){
+    if (error.code === 5) {
         D.failure(D.errorType.AUTHENTICATION_ERROR);
     } else {
         D.failure(D.errorType.GENERIC_ERROR);
@@ -49,8 +35,7 @@ function checkForPasswordError(error){
  * Calls D.success to indicate successful run and setting variable values
 */
 function resultCallback(output, error) {
-    console.info(output)
-    if (error){
+    if (error) {
         checkForPasswordError(error)
     };
     var lines = output.split('\n');
@@ -59,14 +44,13 @@ function resultCallback(output, error) {
     var lastLoginRegex = new RegExp('.*sshd\\[[\\d]+\\]:(.*) port.*');
     lines.forEach(function (line) {
         var match = line.match(lastLoginRegex);
-        if (match){
+        if (match) {
             lastLoginValue = match[1];
         }
     });
-
-    if (!lastLoginValue){
+    if (!lastLoginValue) {
         console.error("Could not parse variable in output", output)
-        D.failure(D.errorType.GENERIC_ERROR)
+        D.failure(D.errorType.PARSING_ERROR)
     } else {
         var lastLoginVariable = D.device.createVariable(
             lastLoginUID, lastLoginLabel, lastLoginValue, lastLoginUnit
@@ -83,13 +67,14 @@ function resultCallback(output, error) {
  * @label Validate Association
  * @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials provided
 */
-function validate(){
-    console.info("Verifying credentials ... ", COMMAND_VALIDATE)
-    sshCommandOptions['command'] = COMMAND_VALIDATE;
+function validate() {
+    var commandValidate = 'ls'
+    console.info("Verifying credentials ... ", commandValidate)
+    sshCommandOptions['command'] = commandValidate;
     D.device.sendSSHCommands(
         sshCommandOptions, resultCallback
     );
-} 
+}
 
 /**
 * @remote_procedure
@@ -97,9 +82,29 @@ function validate(){
 * @documentation This procedure is used to collect 
 * the last user string and store it in a custom driver variable
 */
-function get_status(){
+function get_status() {
     console.info("Getting last login ... ");
-    sshCommandOptions['command'] = COMMAND_GET_STATUS;
+    /**
+     *  'grep' matches expressions 
+     * in the '/var/log/auth.log' file:
+     *'login' for a login at the console;
+    *'accepted' for a successful SSH login;
+    * option '-i' [i]gnores case;
+    * option '-e' gives the [e]xpression
+    * that follows;
+    * '|' pipes the output
+    * as input for the next command; 
+    * 'tail' takes the last lines of input;
+    * option '-n2' takes the last 
+    * '2' [n]umber of lines, as the 
+    * last line will be your current login;
+    * '|' pipes once again;
+    * 'head' takes the first lines of input;
+    * option '-n1' takes the first '1' line 
+    * (the last login before your current one);
+    */
+    var commandLastLogin = 'grep -i -e login -e accepted /var/log/auth.log | grep -v "grep" | tail -n2 | head -n1';
+    sshCommandOptions['command'] = commandLastLogin;
     D.device.sendSSHCommand(
         sshCommandOptions, resultCallback
     );
