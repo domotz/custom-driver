a5e11fa3142c4fb86e2daf70b209f8a0535cf3d2
Mohamed Kallel <mohamed@data4job.com>
2022-08-11 17:15:21 +0100

    docker custom driver

---
+++ examples/ssh/docker_info.js
@@ -0,0 +1,18 @@
+/**
+* @remote_procedure
+* @label Validate Association
+* @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials provided
+*/
+function validate() {
+
+}
+
+
+/**
+* @remote_procedure
+* @label Get Device Variables
+* @documentation This procedure is used for retrieving device * variables data
+*/
+function get_status() {
+  
+}
