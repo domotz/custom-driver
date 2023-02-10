74c2a4ce7d143cc4b8725f366ba857f36a244c0f
Georgi Tsatsev <gtsatsev@domotz.com>
2023-02-10 17:18:40 +0200

    CD-37: Refactor window update driver

---
+++ examples/ssh/windows_pending_reboot_sensor.js
@@ -0,0 +1,119 @@
+/**
+ * Domotz Custom Driver
+ * Name: Windows Pending Reboot Sensor
+ * Description:  Monitors the "Pending Reboot" state [Yes, No] - Reboot action can be performed using the reboot button.
+ * Communication protocol is SSH. Utilizing the native windows powershell command
+ * 
+ * Tested on Windows Versions:
+ *      - Windows 10
+ *      - Microsoft Windows Server 2019
+ * Powershell Version:
+ *      - 5.1.19041.2364
+ * 
+ * Creates two custom driver variables:
+ *      - Device Uptime - The uptime of the device the driver is applied on
+ *      - Pending Reboot - Yes or No, depending if the device has a pending reboot
+ * 
+ * Code used in the command was taken and modified from the PS module "pendingreboot"
+ */
+
+
+var commandGetPendingReboot ="powershell -c \"$invokeWmiMethodParameters = @{Namespace= 'root/default';Class= 'StdRegProv';Name= 'EnumKey';ErrorAction  = 'Stop';};$hklm = [UInt32] '0x80000002';$invokeWmiMethodParameters.ArgumentList = @($hklm, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\');$registryComponentBasedServicing = (Invoke-WmiMethod @invokeWmiMethodParameters).sNames -contains 'RebootPending';$invokeWmiMethodParameters.ArgumentList = @($hklm, 'SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\');$registryWindowsUpdateAutoUpdate = (Invoke-WmiMethod @invokeWmiMethodParameters).sNames -contains 'RebootRequired';$invokeWmiMethodParameters.ArgumentList = @($hklm, 'SYSTEM\CurrentControlSet\Services\Netlogon');$registryNetlogon = (Invoke-WmiMethod @invokeWmiMethodParameters).sNames;$pendingDomainJoin = ($registryNetlogon -contains 'JoinDomain') -or ($registryNetlogon -contains 'AvoidSpnSet');$invokeWmiMethodParameters.Name = 'GetMultiStringValue';$invokeWmiMethodParameters.ArgumentList = @($hklm, 'SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName\', 'ComputerName');$registryActiveComputerName = Invoke-WmiMethod @invokeWmiMethodParameters;$invokeWmiMethodParameters.ArgumentList = @($hklm, 'SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName\', 'ComputerName');$registryComputerName = Invoke-WmiMethod @invokeWmiMethodParameters;$pendingComputerRename = $registryActiveComputerName -ne $registryComputerName -or $pendingDomainJoin;$invokeWmiMethodParameters.ArgumentList = @($hklm, 'SYSTEM\CurrentControlSet\Control\Session Manager\', 'PendingFileRenameOperations');$registryPendingFileRenameOperations = (Invoke-WmiMethod @invokeWmiMethodParameters).sValue;$registryPendingFileRenameOperationsBool = [bool]$registryPendingFileRenameOperations;$isRebootPending = $registryComponentBasedServicing -or $pendingComputerRename -or $pendingDomainJoin -or $registryPendingFileRenameOperationsBool -or $registryWindowsUpdateAutoUpdate;@{ IsRebootPending = $isRebootPending; Uptime =  ((Get-Date) - $(Get-CimInstance -ClassName Win32_OperatingSystem | Select -ExpandProperty LastBootupTime)).Hours}\""
+var sshOptions = {
+    "command": commandGetPendingReboot,
+    "username": D.device.username(),
+    "password": D.device.password(),
+    "timeout": 10000
+};
+
+// Helper function to parse response and call the success callback
+function successCallback(output) {
+    var outputList = output.split(/\r?\n/);
+    var outputLineRebootState = 2;
+    var outputLineDeviceUptime = 3;
+    var pendingRebootLabel = outputList[outputLineRebootState].replace(/\s+/g,' ').trim().split(" ")[0];
+    var pendingRebootValue = outputList[outputLineRebootState].replace(/\s+/g,' ').trim().split(" ")[1];
+    if (pendingRebootValue) {
+        pendingRebootValue = "Yes"
+    } else {
+        pendingRebootValue = "No"
+    }
+    var deviceUptimeLabel = outputList[outputLineDeviceUptime].replace(/\s+/g,' ').trim().split(" ")[0];
+    var deviceUptimeValue = outputList[outputLineDeviceUptime].replace(/\s+/g,' ').trim().split(" ")[1];
+    
+    D.success([
+        D.createVariable('pending-reboot', pendingRebootLabel, pendingRebootValue, null, D.valueType.STRING),
+        D.createVariable('device-uptime-hours', deviceUptimeLabel, deviceUptimeValue, null, D.valueType.NUMBER)
+    ]);
+}
+    
+/**
+* SSH Command execution Callback
+* Checks for errors: Parsing, Authentication, Generic
+* Calls success callback on ssh output
+*/
+function commandExecutionCallback(output, error) {
+    if (error) {
+        console.error("Error: ", error);
+        if (error.message && (error.message.indexOf("Invalid") === -1 || error.message.indexOf("Handshake failed") === -1)) {
+            D.failure(D.errorType.AUTHENTICATION_ERROR);
+        } else {
+            console.error(error);
+            D.failure(D.errorType.GENERIC_ERROR);
+        }
+    } else {
+        if (output && output.indexOf("is not recognized as") !== -1) {
+            D.failure(D.errorType.PARSING_ERROR);
+        } else {
+            successCallback(output);
+        }
+    }
+}
+
+
+/**
+* @remote_procedure
+* @label Validate Association
+* @documentation Verifies if the driver can be applied on the device. Checks for credentials
+*/
+function validate() {
+    console.info("Verifying device can respond correctly to command ... ");
+    D.device.sendSSHCommand(sshOptions, commandExecutionCallback);
+}
+
+/**
+* @remote_procedure
+* @label Get Reboot Pending and Device Uptimne
+* @documentation Pending Reboot Status can be [Yes, No]. Device uptime is in Hours
+*/
+function get_status() {
+    D.device.sendSSHCommand(sshOptions, commandExecutionCallback);
+}
+
+/**
+* @remote_procedure
+* @label Reboot Now
+* @documentation WARNING! This button does not provide you with a confirmation dialogue. It will reboot the computer immediately once pressed.
+*/
+function custom_1(){
+    function rebootCallback(output, error) {
+        if (error) {
+            console.error("Error: ", error);
+            if (error.message && (error.message.indexOf("Invalid") === -1 || error.message.indexOf("Handshake failed") === -1)) {
+                D.failure(D.errorType.AUTHENTICATION_ERROR);
+            } else {
+                console.error(error);
+                D.failure(D.errorType.GENERIC_ERROR);
+            }
+        } else {
+            if (output && output.indexOf("is not recognized as") !== -1) {
+                D.failure(D.errorType.PARSING_ERROR);
+            } else {
+                D.success();
+            }
+        }
+    }
+    var rebootCommand ="powershell -c \"Restart-Computer -Force\""; 
+    sshOptions.command = rebootCommand
+    D.device.sendSSHCommand(sshOptions, rebootCallback);
+}
---
+++ examples/ssh/windows_update.js
@@ -0,0 +1,180 @@
+/**
+ * Domotz Custom Driver 
+ * Name: Windows Update - Count, List, and Install Missing Updates
+ * Description: Shows the number of updates available listed by their severity or category, provides a button to install them on demand
+ * 
+ * Please note that to be able to use the button 'Install Updates' you must be using an Administrative user
+ * please see this online thread: https://social.technet.microsoft.com/Forums/azure/en-US/969b16c7-bd7a-4204-9ae0-b5a5d7663d14/how-to-allow-nonadministrator-install-windows-update?forum=winserverGP
+ * 
+ * Communication protocol is SSH. Utilizing the native windows powershell command
+ * 
+ * Tested on Windows Versions:
+ *      - Windows 10
+ *      - Microsoft Windows Server 2019
+ * Powershell Version:
+ *      - 5.1.19041.2364
+ * 
+ * Creates custom driver variables for missing updates severity counts:
+ *  - Severity Critical
+ *  - Severity Important
+ *  - Severity Low
+ *  - Severity Moderate
+ *  - Severity Unspecified
+ *  - Category Security
+ *
+ * Creates Custom Driver Table with the following columns:
+ *   - Id (Microsoft KB)
+ *   - Title
+ *   - Severity
+ *   - Categories
+ *   - URL (microsoft website)
+ */
+
+
+var getWindowsUpdatesCommand = "powershell -command \"$UpdateSession = New-Object -ComObject Microsoft.Update.Session;$UpdateSearcher = $UpdateSession.CreateupdateSearcher();$Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0 and AutoSelectOnWebSites=1').Updates);$Updates| select  @{n='KB';e={$($_.KBArticleIDs) -join ','}},title,MsrcSeverity,@{n='Category';e={[array](($_.Categories | select Name).Name) -join '|'}}, @{n='URL';e={$($_.moreinfourls) -join ' - '}}|ConvertTo-Json\"";
+var sshOptions = {
+    "command": getWindowsUpdatesCommand,
+    "username": D.device.username(),
+    "password": D.device.password(),
+    "timeout": 40000
+};
+
+// Helper function to parse the windows update info response and call the success callback
+function dataParserCallback(output) {
+    // Creation of custom driver table 
+    var missingUpdatesTable = D.createTable(
+        "Missing Updates",
+        [
+            { label: "Title" },
+            { label: "Severity" },
+            { label: "Categories" },
+            { label: "URL" }
+
+        ]
+    );
+    var CriticalCount = 0;
+    var ImportantCount = 0;
+    var LowCount = 0;
+    var ModerateCount = 0;
+    var UnspecifiedCount = 0;
+    var SecurityCount = 0;
+
+    var k = 0;
+    if (!output) {
+        // If there are no missing updates, we need to update the table accordingly
+        output = '[{"KB" : "None", "Title" : "None","MsrcSeverity" : "None" ,"Category" : "None","URL" : "None"}]'
+    }
+    var jsonOutput = JSON.parse(output);
+    while (k < jsonOutput.length) {
+        var recordId = 'KB' + jsonOutput[k].KB;
+        var title = jsonOutput[k].Title;
+        var severity = jsonOutput[k].MsrcSeverity || 'Unspecified';
+        var category = jsonOutput[k].Category;
+        var url = jsonOutput[k].URL ;
+        
+        if (category.includes("Security")) {
+            SecurityCount++ ;
+        }
+        switch(severity) {
+            case 'Critical':
+                CriticalCount++;
+                break;
+            case 'Important':
+                ImportantCount++;
+                break;
+            case 'Low':
+                LowCount++;
+                break;
+            case 'Moderate':
+                ModerateCount++;
+            break;
+            case 'Unspecified':
+                UnspecifiedCount++;
+                break;
+            default:
+        }
+
+        missingUpdatesTable.insertRecord(
+            recordId, [title, severity, category, url]
+        );
+        k++;
+    }
+    var updateSeverityCounterVariables = [
+        D.createVariable('0-severity-unspecified', 'Severity - Unspecified', UnspecifiedCount, null, D.valueType.NUMBER),
+        D.createVariable('1-severity-moderate', 'Severity - Moderate', ModerateCount, null, D.valueType.NUMBER),
+        D.createVariable('2-severity-low', 'Severity - Low', LowCount, null, D.valueType.NUMBER),
+        D.createVariable('3-severity-important', 'Severity - Important', ImportantCount, null, D.valueType.NUMBER),
+        D.createVariable('4-severity-critical', 'Severity - Critical', CriticalCount, null, D.valueType.NUMBER),
+        D.createVariable('99-category-security', 'Category - Security', SecurityCount, null, D.valueType.NUMBER),
+    ];
+    D.success(updateSeverityCounterVariables,missingUpdatesTable);
+}
+/**
+* SSH Command execution Callback
+* Checks for errors: Parsing, Authentication, Generic
+* Calls success callback on ssh output
+*/
+function commandExecutionCallback(output, error) {
+    if (error) {
+        console.error("Error: ", error);
+        if (error.message && (error.message.indexOf("Invalid") === -1 || error.message.indexOf("Handshake failed") === -1)) {
+            D.failure(D.errorType.AUTHENTICATION_ERROR);
+        } else {
+            console.error(error);
+            D.failure(D.errorType.GENERIC_ERROR);
+        }
+    } else {
+        if (output && output.indexOf("command not found") !== -1) {
+            D.failure(D.errorType.PARSING_ERROR);
+        } else {
+            dataParserCallback(output);
+        }
+    }
+}
+
+/**
+* @remote_procedure
+* @label Validate Association
+* @documentation Verifies if the driver can be applied on the device. Checks for credentials
+*/
+function validate() {
+    D.device.sendSSHCommand(sshOptions, commandExecutionCallback);
+}
+
+/**
+* @remote_procedure
+* @label Get Windows Updates information
+* @documentation Creates varibles for missing updates severity and categories. Creates table for detailed view of missing updates
+*/
+function get_status() {
+    D.device.sendSSHCommand(sshOptions, commandExecutionCallback);
+}
+
+/**
+* @remote_procedure
+* @label Install Updates
+* @documentation Installs all the missing updates from the list. No reboot is triggered upon completion.
+*/
+function custom_1(){
+    function installUpdatesCallback(output, error) {
+        if (error) {
+            console.error("Error: ", error);
+            if (error.message && (error.message.indexOf("Invalid") === -1 || error.message.indexOf("Handshake failed") === -1)) {
+                D.failure(D.errorType.AUTHENTICATION_ERROR);
+            } else {
+                console.error(error);
+                D.failure(D.errorType.GENERIC_ERROR);
+            }
+        } else {
+            if (output && output.indexOf("command not found") !== -1) {
+                D.failure(D.errorType.PARSING_ERROR);
+            } else {
+                D.success();
+            }
+        }
+    }    
+    var installMissingUpdates = "powershell -c \"$UpdateSession = New-Object -ComObject Microsoft.Update.Session;$UpdateSearcher = $UpdateSession.CreateupdateSearcher();$Updates = $UpdateSearcher.Search('IsHidden=0 and IsInstalled=0 and AutoSelectOnWebSites=1').Updates;$Downloader = $UpdateSession.CreateUpdateDownloader();$Downloader.Updates = $Updates;$Downloader.Download();$Installer = New-Object -ComObject Microsoft.Update.Installer;$Installer.Updates = $Updates;$Result = $Installer.Install()\"";
+    sshOptions.command = installMissingUpdates;
+    D.device.sendSSHCommand(sshOptions, installUpdatesCallback);
+
+}
