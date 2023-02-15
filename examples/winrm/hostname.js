328bd7537b2737bb69c3aa80ebe4f779dba52aee
Kalin Stoyanov <kalin.stoyanov@domotz.com>
2023-02-15 09:44:32 +0200

    SSB-2069: update style

--- examples/winrm/hostname.js
+++ examples/winrm/hostname.js
@@ -4,15 +4,17 @@
 * @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials provided
 */
 function validate(){
-    D.device.sendWinRMCommand({command:"Test-WSMan"}, callback_validate);
+    D.device.sendWinRMCommand({command:"Test-WSMan"}, callbackValidate);
 } 
 
-function callback_validate(output){
+function callbackValidate(output) {
     if (output.error === null){
         D.success();
     } 
-    console.error(output.error);
-    D.failure();
+    else {
+        console.error(output.error);
+        D.failure();
+    }
 }
 
 /**
@@ -21,21 +23,23 @@ function callback_validate(output){
 * @documentation This procedure is used for retrieving the device's hostname via WinRM and saving it as a variable
 */
 function get_status(){
-    D.device.sendWinRMCommand({command:"hostname"}, callback_hostname);
+    D.device.sendWinRMCommand({command:"hostname"}, callbackHostname);
 }
 
-function callback_hostname(output) {
+function callbackHostname(output) {
     if (output.error === null){
         var variables = [
-            D.createVariable("hostnameuid", "hostname", extract_stdout(output))
+            D.createVariable("hostnameuid", "hostname", extractStdout(output))
         ];
         D.success(variables);
-    } 
-    console.error(output.error);
-    D.failure();
+    }
+    else {
+        console.error(output.error);
+        D.failure();
+    }
 }
 
-function extract_stdout(output){
+function extractStdout(output) {
     console.debug("Parsing: %s", JSON.stringify(output));
     return output.outcome.stdout;
 }
