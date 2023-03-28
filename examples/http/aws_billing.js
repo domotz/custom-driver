15bb6bd14c09dbe390e123b28faf4bb3a7c09480
Mariem <mariem.mguidich@pivasoftware.com>
2023-03-28 14:03:11 +0100

    Change access variables

--- examples/http/aws_billing.js
+++ examples/http/aws_billing.js
@@ -1,18 +1,17 @@
 var crypto = require("crypto");
 var region = "YOUR_REGION";
-var accessKeyId = "YOUR_ACCESS_KEY_ID";
-var secretAccessKey = "YOUR_SECRET_ACCESS_KEY";
+var accessKey = "YOUR_ACCESS_KEY_ID";
+var secretKey = "YOUR_SECRET_ACCESS_KEY";
 var method = "POST";
 var service = "ce";
-var host = service + "." + region + ".amazonaws.com";
+var host = service + "." + region + ".amazonaws.com:443";
 var path = "/";
 var headers = {
     "Content-Type": "application/x-amz-json-1.0",
-    "X-Amz-Target": "AWSInsightsIndexService.GetCostAndUsage",
+    "X-Amz-Target": "GetCostAndUsage",
     "X-Amz-Date": new Date().toISOString().replace(/[:\-]|\.\d{3}/g, ""),
     "Host": host
 };
-console.log("X-Amz-Date:", headers["X-Amz-Date"]);
 var body = JSON.stringify({
     TimePeriod: {
         Start: "2022-02-01",
@@ -23,10 +22,9 @@ var body = JSON.stringify({
         "BlendedCost"
     ]
 });
-
 var canonicalHeaders = Object.keys(headers)
     .sort()
-    .map(function (key) { key.toLowerCase() + ":" + headers[key] + "\n"; })
+    .map(function (key) { return key.toLowerCase() + ":" + headers[key] + "\n"; })
     .join("");
 var signedHeaders = Object.keys(headers).sort().join(";");
 var canonicalRequest = [method, path, "", canonicalHeaders, signedHeaders, crypto.createHash("sha256").update(body).digest("hex")
@@ -42,7 +40,6 @@ var kRegion = crypto.createHmac("sha256", kDate).update(region).digest();
 var kService = crypto.createHmac("sha256", kRegion).update(service).digest();
 var kSigning = crypto.createHmac("sha256", kService).update("aws4_request").digest();
 var signature = crypto.createHmac("sha256", kSigning).update(stringToSign).digest("hex");
-console.log(signature);
 var authorizationHeader = [
     "AWS4-HMAC-SHA256 Credential=" + accessKey + "/" + credentialScope,
     "SignedHeaders=" + signedHeaders,
