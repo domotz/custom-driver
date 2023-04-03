6db25edad958806ddb7c1eecf0669c973e759334
domotz_aperelli <alessandro.perelli@domotz.com>
2023-04-03 10:55:05 +0200

    folloup with code-review #1

--- examples/winrm/windows_services.js
+++ examples/winrm/windows_services.js
@@ -21,20 +21,18 @@
 
 // This is an example of the filter which suit a Windows 10 computer (workstation):
 // var svcFilter = '@("bits","Dnscache","Spooler","schedule","DHCP Client")'
- 
-// For a server you may want to set the filter as follow :
-var svcFilter = '@("LanmanServer","dnscache","Windows Time","dhcp","schedule","RpcEptMapper","MpsSvc")'
- 
+
 // If you want to list ALL services just put '$null' as a filter 
 // var svcFilter = '$null';
 
+// For a server you may want to set the filter as follow :
+var svcFilter = ["LanmanServer", "dnscache", "Windows Time", "dhcp", "schedule", "RpcEptMapper", "MpsSvc"]
 
-/** Some services have a long DisplayName that may cause a problem with the Domotz Table,
- *  you can use the ServiceName instead setting to false the variable useDisplayName
-**/
-var useDisplayName = false;
+if (svcFilter !== '$null'){
+    svcFilter = '@("' + svcFilter.join('","') + '")';
+}
 
-var getServices = svcFilter + '|Get-Service|Select-Object ServiceName,DisplayName,Status,StartType |ConvertTo-Json'
+var getServices = svcFilter + '|Get-Service|Select-Object ServiceName,DisplayName,Status,StartType | ConvertTo-Json'
 
 // Define the WinRM options when running the commands
 var winrmConfig = {
@@ -43,11 +41,41 @@ var winrmConfig = {
     "password": D.device.password()
 };
 
+
+const statusCodes = {
+    "1": "Stopped",
+    "2": "StartPending",
+    "3": "StopPending",
+    "4": "Running",
+    "5": "ContinuePending",
+    "6": "PausePending",
+    "7": "Paused"
+};
+const startTypes = {
+    "0": "Boot",
+    "1": "System",
+    "2": "Automatic",
+    "3": "Manual",
+    "4": "Disabled"
+};
+
+var svcTable = D.createTable(
+    "Monitored services",
+    [
+
+        { label: "Service Name" },
+        { label: "Status" },
+        { label: "Start Type" }
+
+    ]
+);
+
+
 // Check for Errors on the WinRM command response
-function checkSshError(err) {
-    if(err.message) console.error(err.message);
-    if(err.code == 401) D.failure(D.errorType.AUTHENTICATION_ERROR);
-    if(err.code == 404) D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+function checkWinRmError(err) {
+    if (err.message) console.error(err.message);
+    if (err.code == 401) D.failure(D.errorType.AUTHENTICATION_ERROR);
+    if (err.code == 404) D.failure(D.errorType.RESOURCE_UNAVAILABLE);
     console.error(err);
     D.failure(D.errorType.GENERIC_ERROR);
 }
@@ -55,74 +83,40 @@ function checkSshError(err) {
 /**
 * @remote_procedure
 * @label Validate WinRM is working on device
-* @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials provided
+* @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials and privileges provided
 */
 function validate() {
-    D.device.sendWinRMCommand({ command: "Test-WSMan" }, function (output) {
+    D.device.sendWinRMCommand({ command: "Get-Service eventlog" }, function (output) {
         if (output.error === null) {
             D.success();
         } else {
-            checkSshError(output.error);
+            checkWinRmError(output.error);
         }
     });
 }
 
 /**
 * @remote_procedure
-* @label Get Host failed logon for the last hours
-* @documentation This procedure retrieves last hour failed logon attempts
+* @label Get the selected services data
+* @documentation This procedure retrieves data for the selected services
 */
 function get_status() {
-    D.device.sendWinRMCommand(winrmConfig, callBackFunct);
+    D.device.sendWinRMCommand(winrmConfig, parseOutput);
 }
-
-var svcTable = D.createTable(
-    "Monitored services",
-    [
-
-        { label: "Status" },
-        { label: "StartType" }
-
-    ]
-);
 
 function populateTable(svcName, displayname, status, startType) {
-    const statusCodes = {
-        "1": "Stopped",
-        "2": "StartPending",
-        "3": "StopPending",
-        "4": "Running",
-        "5": "ContinuePending",
-        "6": "PausePending",
-        "7": "Paused"
-    };
-    const startTypes = {
-        "0": "Boot",
-        "1": "System",
-        "2": "Automatic",
-        "3": "Manual",
-        "4": "Disabled"
-    };
-    if (useDisplayName) {
-        svcName = displayname;
-    }
+    var recordID;
+    recordID = displayname.slice(0, 50);
     status = statusCodes[status];
     startType = startTypes[startType]
-    svcTable.insertRecord(svcName, [status, startType]);
+    svcTable.insertRecord(recordID, [svcName, status, startType]);
 }
 
-function callBackFunct(output) {
-
+function parseOutput(output) {
     if (output.error === null) {
         var jsonOutput = JSON.parse(JSON.stringify(output));
         jsonOutput = JSON.parse(jsonOutput.outcome.stdout);
-        if (typeof jsonOutput.length === 'undefined') {
-            populateTable(jsonOutput.ServiceName,
-                jsonOutput.DisplayName,
-                jsonOutput.Status.toString(),
-                jsonOutput.StartType.toString()
-            );
-        } else {
+        if (jsonOutput) {
             var k = 0;
             while (k < jsonOutput.length) {
                 populateTable(jsonOutput[k].ServiceName,
@@ -132,6 +126,12 @@ function callBackFunct(output) {
                 );
                 k++;
             }
+        } else {
+            populateTable(jsonOutput.ServiceName,
+                jsonOutput.DisplayName,
+                jsonOutput.Status.toString(),
+                jsonOutput.StartType.toString()
+            );
         }
         D.success(svcTable);
     } else {
