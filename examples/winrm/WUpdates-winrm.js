add3a4fff28660a6f363b8e5dbcc762cf1125b44
Mariem <mariem.mguidich@pivasoftware.com>
2023-04-20 13:39:41 +0100

    Fix the issue includes because node 0 cannot support it + update comments

--- examples/winrm/WUpdates-winrm.js
+++ examples/winrm/WUpdates-winrm.js
@@ -16,19 +16,32 @@
  * 
  * Privilege required: 
  * - User
- * 
- * 
-**/
-
+ */
 
 var hours = 1;
-
+var CriticalCount = 0;
+var ImportantCount = 0;
+var LowCount = 0;
+var ModerateCount = 0;
+var UnspecifiedCount = 0;
+var SecurityCount = 0;
 
 var winrmConfig = {
-    "command": '',
+    "command": "",
     "username": D.device.username(),
     "password": D.device.password()
 };
+
+// Creation of custom driver table 
+var missingUpdatesTable = D.createTable(
+    "Missing Updates",
+    [
+        { label: "Title" },
+        { label: "Severity" },
+        { label: "Categories" },
+        { label: "URL" }
+    ]
+);
 
 // Check for Errors on the WinRM command response
 function checkWinRmError(err) {
@@ -39,14 +52,13 @@ function checkWinRmError(err) {
     D.failure(D.errorType.GENERIC_ERROR);
 }
 
-
 /**
-* @remote_procedure
-* @label Validate WinRM is working on device
-* @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials provided
-*/
+ * @remote_procedure
+ * @label Validate WinRM is working on device
+ * @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials provided
+ */
 function validate() {
-    winrmConfig.command = 'Get-HotFix';
+    winrmConfig.command = "Get-HotFix";
     D.device.sendWinRMCommand(winrmConfig, function (output) {
         if (output.error === null) {
             D.success();
@@ -57,36 +69,19 @@ function validate() {
 }
 
 /**
-* @remote_procedure
-* @label Get Host failed logon for the last hours
-* @documentation This procedure retrieves last hour failed logon attempts
-*/
+ * @remote_procedure
+ * @label Retrieve list of missing updates
+ * @documentation This procedure retrieves a list of missing updates for the target device
+ */
 function get_status() {
     winrmConfig.command = "$UpdateSession = New-Object -ComObject Microsoft.Update.Session;$UpdateSearcher = $UpdateSession.CreateupdateSearcher();$Updates = @($UpdateSearcher.Search('IsHidden=0 and IsInstalled=0 and AutoSelectOnWebSites=1').Updates);$Updates| select  @{n='KB';e={$($_.KBArticleIDs) -join ','}},title,MsrcSeverity,@{n='Category';e={[array](($_.Categories | select Name).Name) -join '|'}}, @{n='URL';e={$($_.moreinfourls) -join ' - '}}|ConvertTo-Json";
     D.device.sendWinRMCommand(winrmConfig, parseOutput);
 }
 
-
-
-// Creation of custom driver table 
-var missingUpdatesTable = D.createTable(
-    "Missing Updates",
-    [
-        { label: "Title" },
-        { label: "Severity" },
-        { label: "Categories" },
-        { label: "URL" }
-
-    ]
-);
-var CriticalCount = 0;
-var ImportantCount = 0;
-var LowCount = 0;
-var ModerateCount = 0;
-var UnspecifiedCount = 0;
-var SecurityCount = 0;
-
-
+/**
+ * @description Parses the output of the WinRM command and populates the missing updates table and severity counter variables.
+ * @param {object} output - The output of the WinRM command.
+ */
 function parseOutput(output) {
     if (output.error === null) {
         if (output.outcome.stdout) {
@@ -94,34 +89,32 @@ function parseOutput(output) {
             jsonOutput = JSON.parse(jsonOutput.outcome.stdout);
             var k = 0;
             while (k < jsonOutput.length) {
-                var recordId = 'KB' + jsonOutput[k].KB;
+                var recordId = "KB" + jsonOutput[k].KB;
                 var title = jsonOutput[k].Title;
-                var severity = jsonOutput[k].MsrcSeverity || 'Unspecified';
+                var severity = jsonOutput[k].MsrcSeverity || "Unspecified";
                 var category = jsonOutput[k].Category;
                 var url = jsonOutput[k].URL;
-
-                if (category.includes("Security")) {
+                if (category.indexOf("Security")) {
                     SecurityCount++;
                 }
                 switch (severity) {
-                    case 'Critical':
+                    case "Critical":
                         CriticalCount++;
                         break;
-                    case 'Important':
+                    case "Important":
                         ImportantCount++;
                         break;
-                    case 'Low':
+                    case "Low":
                         LowCount++;
                         break;
-                    case 'Moderate':
+                    case "Moderate":
                         ModerateCount++;
                         break;
-                    case 'Unspecified':
+                    case "Unspecified":
                         UnspecifiedCount++;
                         break;
                     default:
                 }
-
                 missingUpdatesTable.insertRecord(
                     recordId, [title, severity, category, url]
                 );
@@ -129,19 +122,16 @@ function parseOutput(output) {
             }
         }
         var updateSeverityCounterVariables = [
-            D.createVariable('0-severity-unspecified', 'Severity - Unspecified', UnspecifiedCount, null, D.valueType.NUMBER),
-            D.createVariable('1-severity-moderate', 'Severity - Moderate', ModerateCount, null, D.valueType.NUMBER),
-            D.createVariable('2-severity-low', 'Severity - Low', LowCount, null, D.valueType.NUMBER),
-            D.createVariable('3-severity-important', 'Severity - Important', ImportantCount, null, D.valueType.NUMBER),
-            D.createVariable('4-severity-critical', 'Severity - Critical', CriticalCount, null, D.valueType.NUMBER),
-            D.createVariable('99-category-security', 'Category - Security', SecurityCount, null, D.valueType.NUMBER),
+            D.createVariable("0-severity-unspecified", "Severity - Unspecified", UnspecifiedCount, null, D.valueType.NUMBER),
+            D.createVariable("1-severity-moderate", "Severity - Moderate", ModerateCount, null, D.valueType.NUMBER),
+            D.createVariable("2-severity-low", "Severity - Low", LowCount, null, D.valueType.NUMBER),
+            D.createVariable("3-severity-important", "Severity - Important", ImportantCount, null, D.valueType.NUMBER),
+            D.createVariable("4-severity-critical", "Severity - Critical", CriticalCount, null, D.valueType.NUMBER),
+            D.createVariable("99-category-security", "Category - Security", SecurityCount, null, D.valueType.NUMBER),
         ];
         D.success(updateSeverityCounterVariables, missingUpdatesTable);
     } else {
         console.error(output.error);
         D.failure();
     }
-
 }
-
-
