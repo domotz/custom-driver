a2a60f36abe4d38289ed4fae55ef65e8bb7332c8
Mariem <mariem.mguidich@pivasoftware.com>
2023-04-25 10:08:51 +0100

    Enhancement in driver Web Service Performance Monitoring

--- examples/http/web_service_performance.js
+++ examples/http/web_service_performance.js
@@ -5,21 +5,30 @@
  *  - Value: The response time for each urls
  */
 
+function sha256(message) {
+    return D.crypto.hash(message, "sha256", null, "hex");
+}
+
 // This is an array of URLs to be tested for response time.
 var urls = ["www.facebook.com", "www.google.com", "www.github.com", "www.domotz.com", "www.twitter.com"];
 
 /**
  * This function measures the response time of a given URL by making an HTTP GET request.
  * @param {string} url  The URL to be tested.
- * @returns {Promise} A promise that resolves with the response time in milliseconds.
  */
-function reponseTime(url) {
+function responseTime(url) {
     var d = D.q.defer();
     var start = new Date();
-    D.device.http.get(url, function (response) {
+    D.device.http.get(url, function (response, error) {
         var end = new Date();
         var responseTime = end - start;
-        d.resolve(responseTime);
+        if (error) {
+            d.resolve(-1);
+        } else if (response.statusCode >= 200 && response.statusCode < 300) {
+            d.resolve(responseTime);
+        } else {
+            d.failure(-1);
+        }
     });
     return d.promise;
 }
@@ -29,24 +38,22 @@ function reponseTime(url) {
  */
 function testUrls() {
     var variables = [];
-    var data = [];
-    urls.forEach(function (url, i) {
-        var resultat = reponseTime(url).then(function (responseTime) {
-            var uid = "id_" + i;
-            var unit = "ms";
-            var value = responseTime;
-            variables.push(
-                D.createVariable(uid, url, value, unit)
-            );
-        });
-        data.push(resultat);
+    urls.forEach(function (url) {
+        responseTime(url)
+            .then(function (responseTime) {
+                var uid = sha256(url).substring(0, 50);
+                var unit = "ms";
+                var value = responseTime;
+                variables.push(
+                    D.createVariable(uid, url, value, unit)
+                );
+                if (variables.length === urls.length) {
+                    D.success(variables);
+                }
+            }).catch(function (error) {
+                console.error(error);
+            });
     });
-    return D.q.all(data)
-        .then(function () {
-            D.success(variables);
-        }).catch(function (error) {
-            console.error(error);
-        });
 }
 
 /**
@@ -55,9 +62,7 @@ function testUrls() {
  * @documentation This procedure is used to validate if the driver can be applied on a device.
  */
 function validate() {
-    testUrls(function () {
-        D.success();
-    });
+    testUrls();
 }
 
 /**
@@ -66,9 +71,5 @@ function validate() {
  * @documentation This procedure is used for testing  the response time of web servecices.
  */
 function get_status() {
-    testUrls(function () {
-        D.success();
-    });
+    testUrls();
 }
-
-
