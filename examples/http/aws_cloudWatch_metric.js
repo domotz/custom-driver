dc3c6c8f5f644e34aa4fe13a9a71ef5c70baff56
Mariem <mariem.mguidich@pivasoftware.com>
2023-05-10 12:06:02 +0100

    Enhancement in driver AWS CloudWAtch etric (Using table format)

--- examples/http/aws_cloudWatch_metric.js
+++ examples/http/aws_cloudWatch_metric.js
@@ -24,7 +24,16 @@ var region = "ADD_REGION";
 var instanceId = "ADD_INSTANCE_ID";
 var dbInstanceId = "ADD_DB_INSTANCE_ID";
 var requestPeriod = 600;
-var monitoringList, metrics;
+var metrics;
+var ec2, rds;
+
+var table = D.createTable("ClouWatch", [
+    { label: "Namespace" },
+    { label: "Type" },
+    { label: "Identifier" },
+    { label: "Metric" },
+    { label: "Value" }
+]);
 
 function sign(key, message) {
     var hex = hmac("sha256", key, message);
@@ -71,35 +80,35 @@ function createMetricsPayload(period, instanceId, dbInstanceId) {
         "CPUUtilization:Percent",
         "DiskQueueDepth:Count",
         "MetadataNoToken:Count",
-
         //For RDS
         "BurstBalance:Percent",
         "WriteLatency:Seconds"
     ];
-
     return metricsList.map(function (metric) {
         var parts = metric.split(":", 2);
         var name = parts[0].replace(/[^a-zA-Z0-9]/g, "");
         // create the EC2 metric object
-        var ec2 = {
-            Id: name.charAt(0).toLowerCase() + name.slice(1),
-            MetricStat: {
-                Metric: {
-                    Namespace: "AWS/EC2",
-                    MetricName: "CPUUtilization",
-                    Dimensions: [
+        ec2 = {
+            "Id": name.charAt(0).toLowerCase() + name.slice(1),
+            "MetricStat": {
+                "Metric": {
+                    "Namespace": "AWS/EC2",
+                    "MetricName": parts[0],
+                    "Dimensions": [
                         {
-                            Name: "InstanceId",
-                            Value: instanceId
+                            "Name": "InstanceId",
+                            "Value": instanceId
                         }
                     ]
                 },
-                Period: period,
-                Stat: "Average"
+                "Period": period,
+                "Stat": "Average",
+                Unit: parts[1]
+
             }
         };
         // create the RDS metric object
-        var rds = {
+        rds = {
             Id: name.charAt(0).toLowerCase() + name.slice(1),
             MetricStat: {
                 Metric: {
@@ -116,7 +125,6 @@ function createMetricsPayload(period, instanceId, dbInstanceId) {
                 Stat: "Average",
                 Unit: parts[1]
             }
-
         };
         return rds;
     });
@@ -202,87 +210,34 @@ function getMetricsData() {
     payload["EndTime"] = endTime;
     return httpPost(prepareParams(payload))
         .then(function (data) {
+            var namespace;
+            var identifier;
+            var type;
+
             metrics = data.GetMetricDataResponse.GetMetricDataResult.MetricDataResults;
+            metrics.forEach(function (item) {
+                if (namespace === "AWS/EC2") {
+
+                    namespace = ec2.MetricStat.Metric.Namespace;
+                    type = ec2.MetricStat.Metric.Dimensions[0].Name;
+                    identifier = instanceId;
+                } else {
+                    namespace = rds.MetricStat.Metric.Namespace;
+                    type = rds.MetricStat.Metric.Dimensions[0].Name;
+                    identifier = dbInstanceId;
+                }
+                var recordId = identifier + "-" + item.Label;
+                var metric = item.Label;
+                var value = item.Values[0];
+                table.insertRecord(recordId, [
+                    namespace,
+                    type,
+                    identifier,
+                    metric,
+                    value !== undefined ? value : ""
+                ]);
+            });
         });
-}
-
-/**
- * @param {string} label  The label to search for in the metric array.
- * @returns A function that takes no arguments and returns the first value associated with the given label, or null if no such label exists.
- */
-function extractValue(label) {
-    return function () {
-        var filteredData = metrics.filter(function (item) { return item.Label === label; });
-        if (filteredData.length === 0 || filteredData[0].Values.length === 0) {
-            return null;
-        }
-        var value = filteredData[0].Values[0];
-        if (typeof value !== "number") {
-            return null;
-        }
-        return value.toFixed(3);
-    };
-}
-
-// The list of custom driver variables to monitor
-function fillConfig() {
-    monitoringList = [
-        {
-            //The percentage of allocated EC2 compute units that are currently in use on the instance.
-            uid: "cpu_utilization",
-            label: "CPU: Utilization",
-            execute: extractValue("CPUUtilization"),
-            unit: "%"
-        },
-        {
-            //The percent of General Purpose SSD (gp2) burst-bucket I/O credits available.
-            uid: "burst_balance",
-            label: "Burst balance",
-            execute: extractValue("BurstBalance"),
-            unit: "%"
-        },
-        {
-            //The number of times the instance metadata service was successfully accessed using a method that does not use a token.
-            uid: "no_token",
-            label: "Metadata: No token",
-            execute: extractValue("MetadataNoToken"),
-        },
-        {
-            //The number of outstanding read/write requests waiting to access the disk.
-            uid: "disk_queue_depth",
-            label: "Disk: Queue depth",
-            execute: extractValue("DiskQueueDepth")
-        },
-        {
-            //The average amount of time taken per disk I/O operation.
-            uid: "write_latency",
-            label: "Disk: Write latencys",
-            execute: extractValue("WriteLatency"),
-            unit: "s"
-        },
-    ];
-}
-
-/**
-* @param {[object]} data array of objects 
-* @returns list of domotz variables
-*/
-function extract(data) {
-    vars = monitoringList.map(function (c) {
-        var result;
-        if (Array.isArray(c.execute)) {
-            result = c.execute.reduce(function (a, b) { return b(a); }, data);
-        } else if (typeof (c.execute) == "function") {
-            result = c.execute(data);
-        }
-        if (result != null) {
-            return D.device.createVariable(c.uid, c.label, result, c.unit, c.type);
-        } else {
-            return null;
-        }
-    }).filter(function (v) {
-        return v != null;
-    });
 }
 
 // This function handles errors
@@ -310,9 +265,7 @@ function validate() {
  */
 function get_status() {
     getMetricsData()
-        .then(fillConfig)
-        .then(extract)
         .then(function () {
-            D.success(vars);
+            D.success(table);
         }).catch(failure);
-}                
+}            
