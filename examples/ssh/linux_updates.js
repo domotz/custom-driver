3ce901704c871a1bf58b99a9799e0e24dcf95b55
Mariem <mariem.mguidich@pivasoftware.com>
2023-05-16 16:06:47 +0100

    Enhancement in driver Linux updates

--- examples/ssh/linux_updates.js
+++ examples/ssh/linux_updates.js
@@ -1,4 +1,5 @@
-/** Name: Linux Updates Monitoring
+/** 
+ * Name: Linux Updates Monitoring
  * Description: Monitors the status of the Updates of a Linux machine
  *   
  * Communication protocol is SSH, using the Linux bash command.
@@ -21,14 +22,14 @@
  * 
 **/
 
-var cmdNumberOfUpdates = "sudo apt update -qq 2>\/dev\/null | sed 's\/[^0-9 ]*\/\/g' > /tmp/updatesno.txt; cat /tmp/updatesno.txt"
-var cmdListOfUpdates="sudo apt update -qq 2>/dev/null | grep -v packages ; sudo apt list --upgradable -qq  2>/dev/null | grep -v 'Listing' | sed 's\/\\\/\/ \/g' | sed 's\/\\[\/ \/g' |  sed 's\/\\]\/ \/g' | awk -F ' ' '{print $1,$7,$3}'"
-var result = []
+var cmdListOfUpdates="sudo apt update -qq 2>/dev/null | grep -v packages ; sudo apt list --upgradable -qq  2>/dev/null | grep -v 'Listing' | sed 's\/\\\/\/ \/g' | sed 's\/\\[\/ \/g' |  sed 's\/\\]\/ \/g' | awk -F ' ' '{print $1,$7,$3}'";
+var result = [];
+
 // SSH options when running the commands
 var sshConfig = {
     username: D.device.username(),
     password: D.device.password(),
-    timeout: 5000
+    timeout: 10000
 };
 
 // SSH promise definition
@@ -44,9 +45,13 @@ function executeCommand(command){
     var d = D.q.defer();
     sshConfig.command = command;
     D.device.sendSSHCommand(sshConfig, function (out, err) {
-        if(err) checkSshError(err);
-        result.push(out)
-        d.resolve();
+        if (err) {
+            checkSshError(err);
+            d.reject(err);
+        } else {
+            result.push(out);
+            d.resolve();
+        }
     });
     return d.promise;
 }
@@ -58,55 +63,47 @@ function executeCommand(command){
 */
 function validate() {
     console.info("Verifying device can respond correctly to command ... ");
-    executeCommand(cmdNumberOfUpdates).then(function(){
-        D.success();
-    });
+    executeCommand(cmdListOfUpdates)
+        .then(function(){
+            D.success();
+        })
+        .catch(function (err) {
+            console.error(err);
+            D.failure(D.errorType.GENERIC_ERROR);
+        });
 }
 
 var updateListTable = D.createTable(
-            "Updates List",
-            [
-                { label: "Pkg Name" },
-                { label: "Old version" },
-                { label: "New version" }
-            ]
-        );
+    "Updates List",
+    [
+        { label: "Pkg Name" },
+        { label: "Old version" },
+        { label: "New version" }
+    ]
+);
  
 function parseData(){
-    var executionResult = result
+    var executionResult = result;
     // executionResult is an array
     // cmdListOfUpdates output is executionResult[0]
-    // cmdNumberOfUpdates output is executionResult[1]
-    
     // parsing cmdListOfUpdates result
     listOfUpdates = executionResult[0];
     var listOfUpdates = listOfUpdates.split(/\r?\n/);
-        for (var i = 0; i < listOfUpdates.length; i++) {
-            var fields = listOfUpdates[i].replace(/\s+/g,' ').trim().split(" ");
-            console.log(fields)
-            var pkgName=fields[0];
-            var pkgOldV=fields[1];
-            var pkgNewV=fields[1];
-            console.log(i+"-"+pkgName.toLowerCase()+"-"+pkgName+"-"+pkgOldV+"-"+pkgNewV)
-
-            updateListTable.insertRecord(
-                i+"-"+pkgName.toLowerCase(), [pkgName, pkgOldV, pkgNewV]
-            );
-        }
-    
-    // parsing cmdNumberOfUpdates result
-    var numberOfUpdatesLabel = "Number of Updates Available"
-    var numberOfUpdateValue = executionResult[1];
-
-    if (!numberOfUpdateValue || /^\s*$/.test(numberOfUpdateValue)) {
-        var error="command output/response is null or empty" ;
-        console.error(error);
-        D.failure(D.errorType.GENERIC_ERROR);
-    } 
-    else {
-        numberOfUpdates = [D.createVariable('updates-number', numberOfUpdatesLabel, numberOfUpdateValue, null, D.valueType.NUMBER)]
-        D.success(numberOfUpdates,updateListTable);
+    for (var i = 0; i < listOfUpdates.length; i++) {
+        var fields = listOfUpdates[i].replace(/\s+/g,' ').trim().split(" ");
+        var pkgName = fields[0];
+        var pkgOldV = fields[1];
+        var pkgNewV = fields[1];
+        var recordId = D.crypto.hash(pkgName, "sha256", null, "hex").slice(0, 50);
+        updateListTable.insertRecord(
+            recordId, [pkgName, pkgOldV, pkgNewV]
+        );
     }
+    var numberOfAvailableUpdatesLabel = "Number of Updates Available";
+    var numberOfAvailableUpdatesValue = listOfUpdates.length;
+    numberOfAvailableUpdates = [D.createVariable("available-updates-number", numberOfAvailableUpdatesLabel, numberOfAvailableUpdatesValue, null, D.valueType.NUMBER)];
+    D.success(numberOfAvailableUpdates,updateListTable);
+
    
 }
 /**
@@ -114,10 +111,11 @@ function parseData(){
 * @label Get Linux Updates
 * @documentation Process data and deliver Linux Updates variables and table
 */
-
 function get_status() {
     executeCommand(cmdListOfUpdates)
-        .then(function(){
-            return executeCommand(cmdNumberOfUpdates)
-        }).then(parseData);
+        .then(parseData)
+        .catch(function (err) {
+            console.error(err);
+            D.failure(D.errorType.GENERIC_ERROR);
+        });
 }
