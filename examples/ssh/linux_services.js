0c198e748f2a18f6e68ec1431dd41083b236d2de
Mariem <mariem.mguidich@pivasoftware.com>
2023-05-19 16:25:36 +0100

    Use include methode

--- examples/ssh/linux_services.js
+++ examples/ssh/linux_services.js
@@ -19,31 +19,31 @@
  * 
 **/
 
-// Define the services you want to show (all|exclude)
-var servicesToGet="all"; 
+// Define the services you want to show (all|include)
+var servicesToGet="include"; 
 
-// if using the "exclude" option you might edit the excludeServices variable which contains the list
+// if using the "include" option you might edit the includeServices variable which contains the list
 // remember to follow this format "'systemd\\|apt'" so each pattern should be followed by \\| as a separator
-// if your and to exclude only one service you should use "'systemd'"
+// if your and to include only one service you should use "'systemd'"
 // PLEASE NOTE THAT the following will not be used if servicesToGet="all"
-var excludeServices="'apt\\|ifup\\|key\\|lvm\\|man\\|networking\\|nfs-config\\|vga\\|emergen'"
-
-if (servicesToGet == "exclude") {
-    var command ="systemctl --all --type=service | sed -e 's/.service//' | tail -n +2 | awk '{print $1,$4}' | head -n -6 | grep -v 'inactive\\|systemd-fsck' | grep -v "+excludeServices
+var includeServices="'systemd\\|apt'";
+var command, error;
+if (servicesToGet == "include") {
+    command = "systemctl --all --type=service | sed -e 's/.service//' | tail -n +2 | awk '{print $1,$4}' | head -n -6 | grep -v 'inactive\\|systemd-fsck' | grep -v " + includeServices;
 }
 else if (servicesToGet == "all") {
-    // we exclude by default ● inactive services and systemd-fsck* services for parsing reasons
-    var command ="systemctl --all --type=service | sed -e 's/.service//' | tail -n +2 | awk '{print $1,$4}' | head -n -6 |  grep -v 'inactive\\|systemd-fsck'"        
+    // we include by default ● inactive services and systemd-fsck* services for parsing reasons
+    command ="systemctl --all --type=service | sed -e 's/.service//' | tail -n +2 | awk '{print $1,$4}' | head -n -6 |  grep -v 'inactive\\|systemd-fsck'";        
 }
 else if (!servicesToGet || /^\s*$/.test(servicesToGet)){
-  var error="servicesToGet variable cannot be null or empty - possible options are: all|exclude" ;
-  console.error(error);
-  D.failure(D.errorType.GENERIC_ERROR);
+    error="servicesToGet variable cannot be null or empty - possible options are: all|include" ;
+    console.error(error);
+    D.failure(D.errorType.GENERIC_ERROR);
 }
 else{
-  var error="servicesToGet variable cannot be set as: "+ servicesToGet + " - possible options are: all|exclude";
-  console.error(error);
-  D.failure(D.errorType.GENERIC_ERROR);
+    error="servicesToGet variable cannot be set as: " + servicesToGet + " - possible options are: all|include";
+    console.error(error);
+    D.failure(D.errorType.GENERIC_ERROR);
 }
 
 // SSH options when running the commands
@@ -54,6 +54,13 @@ var sshConfig = {
     timeout: 5000
 };
 
+var table = D.createTable(
+    "Services List",
+    [
+        { label: "Service Name" },
+        { label: "Status" }
+    ]
+); 
 // SSH promise definition
 function checkSshError(err) {
     if(err.message) console.error(err.message);
@@ -67,8 +74,13 @@ function executeCommand(command){
     var d = D.q.defer();
     sshConfig.command = command;
     D.device.sendSSHCommand(sshConfig, function (out, err) {
-        if(err) checkSshError(err);
-        d.resolve(out);
+        if (err) {
+            checkSshError(err);
+            d.reject(err);
+        }
+        else{
+            d.resolve(out);
+        }
     });
     return d.promise;
 }
@@ -80,9 +92,10 @@ function executeCommand(command){
 */
 function validate() {
     console.info("Verifying device can respond correctly to command ... ");
-    executeCommand(command).then(function(){
-        D.success();
-    });
+    executeCommand(command)
+        .then(function(){
+            D.success();
+        });
 }
  
 /**
@@ -91,24 +104,25 @@ function validate() {
 * @documentation Process data and deliver Linux Services table
 */
 function get_status() {
-    executeCommand(command).then(function(result){
-        var table = D.createTable(
-            "Services List",
-            [
-                { label: "Service Name" },
-                { label: "Status" }
-            ]
-        );        
-        var result = result.split(/\r?\n/);
-        for (var i = 0; i < result.length; i++) {
-            var fields = result[i].replace(/\s+/g,' ').trim().split(" ");
-            console.log(fields)
-            var serviceName=fields[0];
-            var serviceStatus=fields[1];
-            table.insertRecord(
-                i+"-"+serviceName.toLowerCase(), [serviceName, serviceStatus]
-            );
-        }
-        D.success(table);
-    });
+    executeCommand(command)
+        .then(parseOutput)
+        .then(function(){
+            D.success();
+        })
+        .catch(function(error) {
+            checkSshError(error);
+        });
+}
+
+function parseOutput(output){       
+    var result = output.split(/\r?\n/);
+    for (var i = 0; i < result.length; i++) {
+        var fields = result[i].replace(/\s+/g,' ').trim().split(" ");
+        var serviceName = fields[0];
+        var serviceStatus = fields[1];
+        table.insertRecord(
+            i + "-" + serviceName.toLowerCase(), [serviceName, serviceStatus]
+        );
+    }
+    D.success(table);
 }
