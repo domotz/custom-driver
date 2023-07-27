50b8eb9a4ae3492d65c80c2bc0705ebc80c25b34
Mariem <mariem.mguidich@pivasoftware.com>
2023-07-27 16:04:58 +0100

    Add documentation and the script description Json

--- examples/telnet/as400.js
+++ examples/telnet/as400.js
@@ -1,9 +1,26 @@
 
+/**
+ * Domotz Custom Driver 
+ * Name: AS400
+ * Description: This script can monitor the AS400 IBM servers errors list
+ *   
+ * Communication protocol is telnet
+ * 
+ * Tested on AS400 version :
+ *       *BASE   5050     
+ *       *CODE   QSYS      V6R1M1 L00
+ * 
+ * Return a table with this columns:
+ *      - id: Problem ID
+ *      - Codice riferimento sistema: System Reference Code
+ *      - Data e ora di rilevazione: Date and Time of Detection
+**/
 var telnet = D.device.sendTelnetCommand;
-
 var deviceUsername = D.device.username();
 var devicePassword = D.device.password();
-var command = deviceUsername + "\t" + devicePassword + "\r\n\r\nWRKPRB\r\n"
+var command = deviceUsername + "\t" + devicePassword + "\r\n\r\nWRKPRB\r\n";
+
+// Telnet parameters for communication with the AS400 server
 var telnetParams = {
     negotiationMandatory: false,
     shellPrompt: /.*Utente.*/,
@@ -18,13 +35,14 @@ var table = D.createTable(
     ]
 );
 
+
 /**
- * 
- * @returns Promise wait for redis information
+ * Function to get AS400 information via telnet
+ * @returns Promise that waits for the telnet command 
  */
 function getAS400Info() {
     var d = D.q.defer();
-    telnetParams.command = command
+    telnetParams.command = command;
     telnet(telnetParams, function (out, err) {
         if (err) {
             console.error("error while executing command: " + telnetParams.command);
@@ -32,15 +50,19 @@ function getAS400Info() {
         }
         d.resolve(out.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, " "));
     });
-
     return d.promise;
 }
 
+/**
+ * Function to parse errors from AS400 information
+ * @param {[string]} result  list of information returned by the AS400 server
+ * @returns Promise that waits for the error parsing 
+ */
 function errorParsing(result) {
-    var errorCount = result.split("READY").length - 1
-    var errorCheckCmds = Array(errorCount).fill("5\r\n\r\n\x1B[B")
-    var errorCheckCmds = errorCheckCmds.join("")
-    telnetParams.command = command + errorCheckCmds
+    var errorCount = result.split("READY").length - 1;
+    var errorCheckCmds = Array(errorCount).fill("5\r\n\r\n\x1B[B");
+    var errorCheck = errorCheckCmds.join("");
+    telnetParams.command = command + errorCheck;
     var d = D.q.defer();
     telnet(telnetParams, function (out, err) {
         if (err) {
@@ -54,26 +76,26 @@ function errorParsing(result) {
 }
 
 /**
- * 
- * @param {[string]} results list of information returned by redis server
- * @returns monitoring variables
+ * Function to parse AS400 information and populate the table
+ * @param {[string]} results list of information returned by the AS400 server
+ * @returns The populated table with AS400 errors information
  */
 function parseInfo(results) {
-    var srcs = results.match(/SRC[^ ]*/g)
-    var dates = results.match(/\d\d\/\d\d\/\d\d\s+\d\d:\d\d:\d\d/g)
-    var problemIds = results.match(/ID problema[\s\.\:]*\d{10}/g)
+    var srcs = results.match(/SRC[^ ]*/g);
+    var dates = results.match(/\d\d\/\d\d\/\d\d\s+\d\d:\d\d:\d\d/g);
+    var problemIds = results.match(/ID problema[\s\.\:]*\d{10}/g);
     for (var i = 0; i < problemIds.length; i++) {
-        var problemIdRow = problemIds[i].split(" ")
+        var problemIdRow = problemIds[i].split(" ");
         table.insertRecord(problemIdRow[problemIdRow.length - 1], [
             srcs[i],
             dates[i]
-        ])
+        ]);
     }
-    return table
+    return table;
 
 }
 
-
+// Function to handle errors
 function failure(err) {
     console.error(err);
     D.failure(D.errorType.GENERIC_ERROR);
@@ -91,11 +113,10 @@ function validate() {
 
 }
 
-
 /**
 * @remote_procedure
 * @label Get Device Variables
-* @documentation This procedure is used for retrieving device * variables data
+* @documentation This procedure is used for retrieving the status of AS400 server errors 
 */
 function get_status() {
     getAS400Info()
