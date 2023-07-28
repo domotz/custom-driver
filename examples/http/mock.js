05fed2f1d7977c5458af398aa4bc3310e4c0924e
Kalin Stoyanov <kalin.stoyanov@domotz.com>
2023-07-28 17:03:49 +0300

    SSB-2518: extend mock template with parameters

--- examples/http/mock.js
+++ examples/http/mock.js
@@ -3,8 +3,10 @@ function validate(){
 };
 function get_status(){
     var d = new Date();
+    testParam = D.getParameter("testParam");
     var myVariable1 = D.device.createVariable("seconds", "Second", d.getSeconds(), "seconds");
     var myVariable2 = D.device.createVariable("month", "Month", d.getMonth(), "months");
     var myVariable3 = D.device.createVariable("day", "Day", d.getDay(), "days");
-    D.success([myVariable1, myVariable2, myVariable3]);
+    var myVariable4 = D.device.createVariable("p", "Param", testParam);
+    D.success([myVariable1, myVariable2, myVariable3, myVariable4]);
 }
