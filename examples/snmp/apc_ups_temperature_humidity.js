f089704445e6237d884577d8140f42fb343189ac
Mariem <mariem.mguidich@pivasoftware.com>
2023-08-08 09:57:17 +0100

    Fix error case handling and validate function

--- examples/snmp/apc_ups_temperature_humidity.js
+++ examples/snmp/apc_ups_temperature_humidity.js
@@ -3,8 +3,10 @@
  * Communication protocol is SNMP V2
  * Creates a custom driver variable:
  *      - Probe Name: The descriptive name for the probe
- *      - Room Temperature: The current temperature reading from the probe
- *      - Room Humidity: The current humidity reading from the probe in percent
+ *      - Probe Temperature: The current temperature reading from the probe
+ *      - Probe Humidity: The current humidity reading from the probe in percent
+ * 
+ * Tested under APC Smart-UPS X 3000 version 6.8.2
  */
     
 var iemStatusProbesTableOID = "1.3.6.1.4.1.318.1.1.10.2.3.2";
@@ -21,12 +23,11 @@ function validateAndGetData() {
             D.failure(D.errorType.GENERIC_ERROR);
         } else if (!out) {
             D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+        } else if (!(probNameOID in out) || !(probeTempOID in out) || !(probeTempUnitsOID in out) || !(probeHumidityOID in out)) {
+            console.error("Missing necessary OID valuvares in SNMP response");
+            D.failure(D.errorType.PARSING_ERROR);
         }  else {
-            if (err || out[probNameOID].err || out[probeTempOID].err || out[probeTempUnitsOID].err || out[probeHumidityOID].err) { 
-                D.failure(D.errorType.PARSING_ERROR);
-            } else {
-                d.resolve(out);
-            }
+            d.resolve(out);
         }
     });
     return d.promise;
@@ -39,9 +40,7 @@ function validateAndGetData() {
  */
 function validate(){
     validateAndGetData()
-        .then(function(){
-            D.success();
-        })
+        .then(D.success)
         .catch(function (err) {
             console.error(err);
             D.failure(D.errorType.GENERIC_ERROR);
