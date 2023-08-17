003650225dcb47de20f15749d19a1fa2be4a38f8
Kalin Stoyanov <kalin.stoyanov@domotz.com>
2023-08-17 17:47:07 +0300

    SSB-2570: bump mock version

--- examples/http/mock.js
+++ examples/http/mock.js
@@ -4,7 +4,7 @@ function validate(){
 function get_status(){
     var d = new Date();
     testParam = D.getParameter("testParam");
-    var myVariable1 = D.device.createVariable("seconds", "Second", d.getSeconds(), "seconds");
+    var myVariable1 = D.device.createVariable("secondss", "Second", d.getSeconds(), "seconds");
     var myVariable2 = D.device.createVariable("month", "Month", d.getMonth(), "months");
     var myVariable3 = D.device.createVariable("day", "Day", d.getDay(), "days");
     var myVariable4 = D.device.createVariable("p", "Param", testParam);
