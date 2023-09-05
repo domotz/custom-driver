57270e5aac6f483ccacbafe22dc5298e6a33d98c
Lorenzo Allori <l.allori@domotz.com>
2023-09-05 16:30:27 +0200

    little fixes to script

--- examples/winrm/microsoft_exchange.js
+++ examples/winrm/microsoft_exchange.js
@@ -8,11 +8,11 @@
  * Tested on Windows Versions:
  *      - Windows 10
  *      - Microsoft Exchange Server 2019 CU12
+ * 
  * Powershell Version:
  *      - 5.1.19041.2364
  * 
  * Creates a Custom Driver Table with the following columns:
- * 
  *      - Id: Performance counter path
  *      - Instance Name: Instance name of the counter
  *      - Cooked Value: Cooked value of the counter
@@ -43,34 +43,34 @@ var counters = [
 
 // Counter type mappings
 var counterTypeMappings = {
-    0: "NumberOfItemsHEX32",
-    256: "NumberOfItemsHEX64",
-    65536: "NumberOfItems32",
-    65792: "NumberOfItems64",
-    4195328: "CounterDelta32",
-    4195584: "CounterDelta64",
-    4260864: "SampleCounter",
-    4523008: "CountPerTimeInterval32",
-    4523264: "CountPerTimeInterval64",
-    272696320: "RateOfCountsPerSecond32",
-    272696576: "RateOfCountsPerSecond64",
-    537003008: "RawFraction",
-    541132032: "CounterTimer",
-    542180608: "Timer100Ns",
-    549585920: "SampleFraction",
-    557909248: "CounterTimerInverse",
-    558957824: "Timer100NsInverse",
-    574686464: "CounterMultiTimer",
-    575735040: "CounterMultiTimer100Ns",
-    591463680: "CounterMultiTimerInverse",
-    592512256: "CounterMultiTimer100NsInverse",
-    805438464: "AverageTimer32",
-    807666944: "ElapsedTime",
-    1073874176: "AverageCount64",
-    1073939457: "SampleBase",
-    1073939458: "AverageBase",
-    1073939459: "RawBase",
-    1107494144: "CounterMultiBase"
+    0: "number", //NumberOfItemsHEX32
+    256: "number.", //NumberOfItemsHEX64
+    65536: "number", // NumberOfItems32
+    65792: "number", // NumberOfItems64
+    4195328: "CounterDelta32", //
+    4195584: "CounterDelta64", //
+    4260864: "SampleCounter", //
+    4523008: "CountPerTimeInterval32", //
+    4523264: "CountPerTimeInterval64", //
+    272696320: "rate/s", //RateOfCountsPerSecond32
+    272696576: "rate/s", //RateOfCountsPerSecond64
+    537003008: "RawFraction", //
+    541132032: "CounterTimer", //
+    542180608: "Timer100Ns", //
+    549585920: "SampleFraction", //
+    557909248: "CounterTimerInverse", //
+    558957824: "Timer100NsInverse", //
+    574686464: "CounterMultiTimer", //
+    575735040: "CounterMultiTimer100Ns", //
+    591463680: "CounterMultiTimerInverse", //
+    592512256: "CounterMultiTimer100NsInverse", //
+    805438464: "average time", //AverageTimer32
+    807666944: "elapsed time", //ElapsedTime
+    1073874176: "average count", //AverageCount64
+    1073939457: "SampleBase", //
+    1073939458: "AverageBase", //
+    1073939459: "RawBase", //
+    1107494144: "CounterMultiase" //
 };
 
 // WinRM configuration
@@ -83,11 +83,11 @@ var winrmConfig = {
 
 // Create a table to store monitored data
 var table = D.createTable(
-    "Microsoft Exchanges",
+    "MS Exchange Monitored Items",
     [
         { label: "Instance Name" },
-        { label: "Cooked Value" },
-        { label: "Counter Type" }
+        { label: "Value" },
+        { label: "Unit" }
     ]
 );
 
@@ -134,11 +134,12 @@ function parseOutput(output) {
         var jsonOutput = JSON.parse(JSON.stringify(JSON.parse(output.outcome.stdout)));
         while (k < jsonOutput.length) {
             var path = jsonOutput[k].Path;
+            const msitem = path.replace(/\\\\(.*?)\\/g,"").replace(/\\/g, " ").replace("msexchange", "");
+            var recordId = ((k + 1) + "-" + msitem).slice(0,50);
             var instanceName = jsonOutput[k].InstanceName || "-";
             var cookedValue = jsonOutput[k].CookedValue;
             var counterTypeCode = jsonOutput[k].CounterType;
             var counterType = counterTypeMappings[counterTypeCode] || "Unknown Counter Type";           
-            var recordId = ("MSEX" + (k + 1) + ": " + path).slice(0,50);
             table.insertRecord(recordId, [instanceName, cookedValue, counterType]); 
             k++;
         }
