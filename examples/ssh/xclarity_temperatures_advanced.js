8454a27c4ceb6e683c2bf7b5ed08d06d4b708983
mariemm97 <mariem.mguidich@pivasoftware.com>
2023-09-26 18:15:12 +0100

    Add driver XClarity (Lenovo) Temperatures (advanced) and script description JSON

---
+++ examples/ssh/xclarity_temperatures_advanced.js
@@ -0,0 +1,114 @@
+/**
+ * Name: XClarity Temperatures 
+ * Description: This script can monitor temperature information on a Lenovo XClarity server
+ *             
+ * Communication protocol is SSH.
+ * 
+ * Tested under Lenovo XClarity version 8.42
+ * 
+ * Keyboard Interactive option: true/false (depends on XClarity version).
+ * 
+ * Creates a Custom Driver table with the following columns:
+ *      - Temperature: Current value
+ *      - Warning Reset: Positive-going Threshold Hysteresis value
+ *      - Warning: Upper non-critical Threshold
+ *      - Soft Shutdown: Upper critical Threshold
+ *      - Hard Shutdown: Upper non-recoverable Threshold
+ */
+
+// Define the command to be executed on the XClarity server
+var command = "temps";
+
+var sshConfig = {
+    "username": D.device.username(),
+    "password": D.device.password(),
+    "timeout": 100000,
+    "keyboard_interactive": true
+};
+
+// Create a custom table to store temperature information
+var temperaturesTable = D.createTable(
+    "Temperatures",
+    [
+        { label: "Temperature"},
+        { label: "Warning Reset"},      
+        { label: "Warning"},
+        { label: "Soft Shutdown"},
+        { label: "Hard Shutdown"}
+    ]
+);
+
+// Function to handle SSH errors and failure cases
+function checkSshError(err) {
+    if(err.message) console.error(err.message);
+    if(err.code == 5) D.failure(D.errorType.AUTHENTICATION_ERROR);
+    if(err.code == 255) D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+    console.error(err);
+    D.failure(D.errorType.GENERIC_ERROR);
+}
+
+// Function to execute an SSH command
+function executeCommand(command) {
+    var d = D.q.defer();
+    sshConfig.command = command;
+    D.device.sendSSHCommand(sshConfig, function (output, err) {
+        if (err) {
+            checkSshError(err);
+        } else {
+            d.resolve(output);
+        }                  
+    });
+    return d.promise;
+}
+
+/**
+ * @remote_procedure
+ * @label Validate Connection 
+ * @documentation Validates the connection and command execution
+ */
+function validate() {
+    executeCommand(command)
+        .then(parseValidateOutput)
+        .then(D.success)
+        .catch(checkSshError);
+}
+
+function parseValidateOutput(output) {
+    if (output.trim() !== "") {
+        console.info("Validation successful");
+    } 
+}
+
+/**
+ * @remote_procedure
+ * @label Get Temperature Information
+ * @documentation Retrieves and parses temperature information from the server.
+ */
+function get_status() {
+    executeCommand(command)
+        .then(parseData)
+        .catch(checkSshError);
+}
+
+// Parse and insert temperature data into the custom table
+function parseData(output) {
+    var lines = output.split("\n");
+    var recordIdReservedWords = ['\\?', '\\*', '\\%', 'table', 'column', 'history'];
+    var recordIdSanitisationRegex = new RegExp(recordIdReservedWords.join('|'), 'g');
+    for (var i = 3; i < lines.length-1; i++) {
+        var line = lines[i].trim();
+        var parts = line.split(/\s+/);
+        var temperature = parts.slice(-3)[0];
+        var warningReset = parts.slice(-5)[0];
+        var warning = parts.slice(-4)[0];
+        var softShutdown = parts.slice(-2)[0];
+        var hardShutdown = parts.slice(-1)[0];
+        var name = (parts.slice(0, -5).join(" ")).replace(/ /g, '-');
+        var recordId = name.replace(recordIdSanitisationRegex, '').slice(0, 50);
+        temperaturesTable.insertRecord(
+            recordId, [temperature, warningReset, warning, softShutdown, hardShutdown]
+        );
+    }
+    D.success(temperaturesTable);
+}
+
