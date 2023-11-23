b972f8bbab8a257f2eed11ee0d28eed930c1cb20
mariemm97 <mariem.mguidich@pivasoftware.com>
2023-11-23 09:19:43 +0100

    Enhancement in script ClearOne Converge

--- examples/http/clearone_convergence.js
+++ examples/http/clearone_convergence.js
@@ -12,13 +12,13 @@
  *    - apiKey: Communicate with http api using API-X-KEY
  *    
  * Custom Driver Variables:
- *    - Model: The model device.
- *    - Serial Number: The serial number device.
+ *    - Model: The model device
+ *    - Serial Number: The serial number device
  *    - Firmware Version: Firmware version in device. (When * is shown means firmware pending update)
- *    - Locate Mode: True when the device is mode locate, false otherwise.
+ *    - Locate Mode: True when the device is mode locate, false otherwise
  *    - Status: Status device (STATUS - TIME IN THIS STATUS)
- *    - Location: Location device.
- *    - Part Number: Part number device.
+ *    - Location: Location device
+ *    - Part Number: Part number device
  *
  **/
 
@@ -27,6 +27,7 @@ var urlCl = D.getParameter('urlCl');
 
 // API Key access to Convergence
 var apiKey = D.getParameter('apiKey');
+var deviceInfo , detailInfo;
 
 /**
  * @param {string} url  The URL to perform the GET request
@@ -63,27 +64,28 @@ function httpGet(url) {
     return d.promise;
 }
 
-// Get device information
-function getData() {
-    var output = {};
+/**
+ * @returns promise for http response body containig device information
+ */
+function search() {
     return httpGet("/dashboard-back/api/devices/pro-audio/search?q=" + D.device.ip())
-          .then(function (data) {
-            output.deviceInfo = data;
-            if (data.avDevices && data.avDevices.length > 0) {
-                return httpGet("/dashboard-back/api/devices/pro-audio/" + data.avDevices[0].id)
-                    .then(function (result) {
-                        output.partNumberInfo = result;
-                        return output;
-                    })
-            } else {
-                console.error("No data available");
-                D.failure(D.errorType.GENERIC_ERROR);
-            }
-        })
+        .then(function(data){
+            deviceInfo = data.avDevices[0];
+        });
 }
 
-function extractVariables(data) {
-    var deviceInfo = data.deviceInfo.avDevices[0];
+/**
+ * @returns promise for http response body containig detailed information
+ */
+function getDetail() {
+    return httpGet("/dashboard-back/api/devices/pro-audio/" + deviceInfo.id)
+        .then(function(output){
+            detailInfo = output;
+        });
+}
+
+// Function to extract variables
+function extractVariables() {
     var firmware = deviceInfo.firmwareVersion;
     if (deviceInfo.hasNewFirmware) {
         firmware = firmware + ' *';
@@ -95,7 +97,7 @@ function extractVariables(data) {
         D.createVariable("locate-mode", "Locate Mode", deviceInfo.locateMode),
         D.createVariable("relative-status", "Status", deviceInfo.relativeStatus),
         D.createVariable("location", "Location", deviceInfo.location),
-        D.createVariable("part-number", "Part Number", data.partNumberInfo.partNumber)
+        D.createVariable("part-number", "Part Number", detailInfo.partNumber)
     ];
     D.success(variables);
 }
@@ -106,7 +108,7 @@ function extractVariables(data) {
  * @documentation This procedure is used to validate if data is accessible
  */
 function validate() {
-    getData()
+    search()
         .then(D.success)
         .catch(function (err) {
             console.error(err);
@@ -120,7 +122,8 @@ function validate() {
  * @documentation This procedure is used to extract information for ClearOne devices
  */
 function get_status() {
-    getData()
+    search()
+        .then(getDetail)
         .then(extractVariables)
         .catch(function (err) {
             console.error(err);
