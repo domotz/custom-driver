67ca0bc3ae52b0bdbdbad0da26dce0db44f82bff
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-02-28 15:13:06 +0100

    Enhance Dell PowerVault SAN Controllers script

--- examples/http/dell_power_vault_san_controllers.js
+++ examples/http/dell_power_vault_san_controllers.js
@@ -13,8 +13,6 @@
  * 
  **/
 
-var sessionToken;
-
 // Create a Custom Driver table to store controller information
 var table = D.createTable(
     "Controllers",[
@@ -22,29 +20,6 @@ var table = D.createTable(
         { label: "Health", valueType: D.valueType.STRING }
     ]
 );
-
-// Process the response from the server
-function processResponse(d) {
-    return function process(error, response, body) {
-        console.log(response);
-        if (error) {          
-            console.error(error);
-            D.failure(D.errorType.GENERIC_ERROR);
-        } 
-        if (response.headers["x-auth-token"]) {
-            sessionToken = response.headers["x-auth-token"];
-        } else if (response.headers["command-status"] && response.headers["command-status"].indexOf("Command failed") !== -1) {
-            console.error("Session token not found in response headers");
-            D.failure(D.errorType.AUTHENTICATION_ERROR);
-        } else if (response.headers["command-status"] && response.headers["command-status"].indexOf("Invalid URL") !== -1) {
-            console.error("Invalid URL");
-            D.failure(D.errorType.RESOURCE_UNAVAILABLE);
-        } else if (response.statusCode !== 200) {
-            D.failure(D.errorType.GENERIC_ERROR);
-        }         
-        d.resolve(JSON.parse(body));
-    };
-}
 
 /**
  * Logs in to the Dell PowerVault SAN system
@@ -61,45 +36,87 @@ function login() {
             "UserName": D.device.username(),
             "Password": D.device.password() 
         })
-
     };
-    D.device.http.post(config, processResponse(d));
-    return d.promise;
-}
-
-// Function to make an HTTP GET request to retrieve controllers from the Dell PowerVault SAN system
-function getControllers() {
-    var d = D.q.defer();
-    var config = {
-        url: "/redfish/v1/Storage",
-        protocol: "https",
-        jar: true,
-        rejectUnauthorized: false,
-        headers: {
-            'X-Auth-Token': sessionToken,
-        }
-    };
-    D.device.http.get(config, processResponse(d));
+    D.device.http.post(config, function(error, response, body){
+        if (error) {  
+            console.error(error);
+            D.failure(D.errorType.GENERIC_ERROR);                     
+        } else {
+            if (response.headers && response.headers["command-status"] && response.headers["command-status"].indexOf("Invalid session key") !== -1) {
+                console.error("Invalid session key found in response headers");
+                D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+            }
+            if (response.headers && response.headers["command-status"] && response.headers["command-status"].indexOf("Command failed") !== -1) {
+                console.error("Command failed found in response headers");
+                D.failure(D.errorType.AUTHENTICATION_ERROR);
+            }
+            
+            var sessionToken = response.headers["x-auth-token"];
+            d.resolve(sessionToken);
+        }            
+    });
     return d.promise;
 }
 
 /**
- * Function to make an HTTP GET request to retrieve information about a specific controller
- * @param {*} memberUrl The URL of the controller
+ * Sends an HTTP GET request
+ * @param {string} url The URL to perform the GET request
+ * @param {string} sessionToken The session token for authentication
+ * @returns A promise that resolves with the HTTP response body
  */
-function getControllerInfo(memberUrl) {
+function httpGet(url, sessionToken) {
     var d = D.q.defer();
     var config = {
-        url: memberUrl,
+        url: url,
         protocol: "https",
         jar: true,
         rejectUnauthorized: false,
         headers: {
-            'X-Auth-Token': sessionToken,
+            'X-Auth-Token': sessionToken
         }
     };
-    D.device.http.get(config, processResponse(d));
+    D.device.http.get(config, function(error, response, body){
+        if (error) {
+            console.error(error);
+            D.failure(D.errorType.GENERIC_ERROR);  
+        }
+        if (response.statusCode !== 200) {
+            console.error(error);
+            D.failure(D.errorType.GENERIC_ERROR);  
+        }
+        d.resolve(JSON.parse(body));
+    });
     return d.promise;
+}
+
+// Function to make an HTTP GET request to retrieve controllers from the Dell PowerVault SAN system
+function getControllers(sessionToken){
+    return httpGet("/redfish/v1/Storage", sessionToken)
+        .then(function(controllers){
+            if (controllers && controllers.Members) {
+                var promises = controllers.Members.map(function (member) {
+                    return getControllerInfo(member['@odata.id'], sessionToken);
+                });
+                return D.q.all(promises);
+            } else {
+                console.error("Invalid response or missing Members array");
+                D.failure(D.errorType.GENERIC_ERROR);                     
+
+            } 
+        })
+        .catch(function(err) {
+            console.error("Error retrieving controller info " + err);
+            D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+        });
+}
+
+/**
+ * Retrieves information about a specific controller
+ * @param {string} controllerUrl The URL of the controller
+ * @returns A promise representing the retrieval of controller information
+ */
+function getControllerInfo(controllerUrl, sessionToken) {
+    return httpGet(controllerUrl, sessionToken);
 }
 
 
@@ -128,6 +145,7 @@ function extractData(controllerInfos) {
             });
         } else {
             console.error("No information available");
+            D.failure(D.errorType.GENERIC_ERROR);
         }
     });
     D.success(table);
@@ -143,12 +161,12 @@ function validate(){
     login()
         .then(getControllers)
         .then(function (response) {
-            if (response && response["@odata.id"].indexOf("/redfish/v1/Storage") !== -1) {
-                console.info("Data available");
+            if (response && response.length > 0) {
+                console.log("Validation successful");
                 D.success();
             } else {
-                console.error("Desired link not found");
-                D.failure(D.errorType.GENERIC_ERROR);
+                console.error("Validation failed");
+                D.failure(D.errorType.PARSING_ERROR);
             }
         })
         .catch(function (err) {
@@ -165,17 +183,6 @@ function validate(){
 function get_status() {
     login()
         .then(getControllers)
-        .then(function (response) {
-            if (response && response.Members) {
-                var promises = response.Members.map(function (member) {
-                    return getControllerInfo(member['@odata.id']);
-                });
-                return D.q.all(promises);
-            } else {
-                console.error("No controller information found");
-                D.failure(D.errorType.GENERIC_ERROR);
-            }
-        })
         .then(extractData)
         .catch(function (err) {
             console.error(err);
