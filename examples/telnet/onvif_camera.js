777f03958433f0cc2a41d18511b5cc7a13eac2ef
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-05-13 19:16:46 +0100

    CD-375 Update script to use HTTP protocol for getting profiles and stream URIs.

--- examples/telnet/onvif_camera.js
+++ examples/telnet/onvif_camera.js
@@ -3,7 +3,7 @@
  * Name: ONVIF camera monitoring
  * Description: Monitors the video capturing status of an ONVIF camera to check if capturing is still working or has stopped.
  * 
- * Communication protocol is telnet
+ * Communication protocols is Telnet and HTTP
  * 
  * Tested on LILIN camera model LD2222
  *
@@ -20,20 +20,9 @@ var authString = username + ":" + password;
 var base64AuthString = D._unsafe.buffer.from(authString).toString('base64');
 
 // HTTP request parameters
-var method = "POST";
-var endpoint = "/onvif/device_service";
-var host = D.device.ip();
-
-//Http command to send requests to the ONVIF device 
-var command = method + ' ' + endpoint + ' HTTP/1.1\r\nHost: ' + host + '\r\nAuthorization: Basic ' + base64AuthString + '\r\n\r\n';
+var url = "/onvif/device_service";
 
 var filterProfileName = D.getParameter("profileName");
-
-//HTttp parameters 
-var httpParams = {
-    port: 80,
-    negotiationMandatory: false,
-};
 
 // Telnet parameters
 var telnetParams = {
@@ -41,33 +30,42 @@ var telnetParams = {
     port: 554
 };
 
+// Table for capturing status
+var table = D.createTable(
+    "Video Streaming Status",
+    [
+        { label: "Streaming status" }
+    ]
+);
+
 /**
  * Retrieves the profiles from the ONVIF camera.
  * @returns A promise that resolves with an array of profile tokens.
  */
 function getProfiles() {
     var d = D.q.defer();
-    // SOAP request to retrieve profiles
-    var xmlProfiles = '<?xml version="1.0" encoding="UTF-8"?>\r\n' + 
-                      '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl">\r\n' + 
-                      '<soap:Body>\r\n' + 
-                      '<trt:GetProfiles/>\r\n' + 
-                      '</soap:Body>\r\n' + 
-                      '</soap:Envelope>\r\n';
-
-    httpParams.command = command + xmlProfiles;
-    D.device.sendTelnetCommand(httpParams, function (out, err) {
-        if (err) {
-            failure(err);
-        }
-        else if (!out) {
+    var config = {
+        url: url,
+        username: username,
+        password: password,
+        auth: "basic",
+        jar: true,
+        rejectUnauthorized: false,
+        body: '<?xml version="1.0" encoding="UTF-8"?>\n<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl">\n<soap:Body>\n<trt:GetProfiles/>\n</soap:Body>\n</soap:Envelope>' 
+    };
+    D.device.http.post(config, function(error, response, body){
+        if (error) {          
+            console.error(error);
+            D.failure(D.errorType.GENERIC_ERROR);
+        } else if (!response) {
             D.failure(D.errorType.RESOURCE_UNAVAILABLE);
-        }
-        else if (out.indexOf("The action requested requires authorization and the sender is not authorized") != -1) {
+        } else if (response.statusCode == 400) {
             D.failure(D.errorType.AUTHENTICATION_ERROR);
-        } 
-        else {
-            var $ = D.htmlParse(out);
+        } else if (response.statusCode != 200) {
+            D.failure(D.errorType.GENERIC_ERROR);
+        } else {
+
+            var $ = D.htmlParse(body);
             var profiles = $("trt\\:Profiles"); 
             var profileTokens = [];
             profiles.each(function(index, element) {
@@ -86,41 +84,53 @@ function getProfiles() {
  * @returns A promise that resolves with an array of stream URIs
  */
 function getStreamURIs(profileTokens){
-    var xmlStreamUris = '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl">';
-        xmlStreamUris += '<soap:Body>';
+    var xmlPayload = '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl"><soap:Body>';
 
     var promises = [];
-
+   
     profileTokens.forEach(function(profileToken) {
         var d = D.q.defer();
-        var streamUris = xmlStreamUris + '<trt:GetStreamUri>' +
-                                         '<trt:StreamSetup>' +
-                                         '<trt:Stream xmlns:trt="http://www.onvif.org/ver10/media/wsdl">RTP-Unicast</trt:Stream>' +
-                                         '<trt:Transport xmlns:trt="http://www.onvif.org/ver10/media/wsdl">' +
-                                         '<trt:Protocol>RTSP</trt:Protocol>' +
-                                         '</trt:Transport>' +
-                                         '</trt:StreamSetup>' +
-                                         '<trt:ProfileToken>' + profileToken + '</trt:ProfileToken>' +
-                                         '</trt:GetStreamUri>' +
-                                         '</soap:Body>' +
-                                         '</soap:Envelope>';
+        var streamUris = xmlPayload + '<trt:GetStreamUri>' +
+                                      '<trt:StreamSetup>' +
+                                      '<trt:Stream xmlns:trt="http://www.onvif.org/ver10/media/wsdl">RTP-Unicast</trt:Stream>' +
+                                      '<trt:Transport xmlns:trt="http://www.onvif.org/ver10/media/wsdl">' +
+                                      '<trt:Protocol>RTSP</trt:Protocol>' +
+                                      '</trt:Transport>' +
+                                      '</trt:StreamSetup>' +
+                                      '<trt:ProfileToken>' + profileToken + '</trt:ProfileToken>' +
+                                      '</trt:GetStreamUri>' +
+                                      '</soap:Body>' +
+                                      '</soap:Envelope>';
 
-        httpParams.command = command + streamUris;
+        var config = {
+            url: url,
+            username: username,
+            password: password,
+            auth: "basic",
+            jar: true,
+            rejectUnauthorized: false,
+            body: streamUris
+        };
+                                     
+        D.device.http.post(config, function(error, response, body){
 
-        D.device.sendTelnetCommand(httpParams, function(out, err) {
-            if (err) {
-                d.reject(err);
-            } else if (!out) {
-                d.reject(D.errorType.RESOURCE_UNAVAILABLE);
-            } else if (out.indexOf("The action requested requires authorization and the sender is not authorized") !== -1) {
-                d.reject(D.errorType.AUTHENTICATION_ERROR);
+            if (error) {          
+                console.error(error);
+                D.failure(D.errorType.GENERIC_ERROR);
+            } else if (!response) {
+                D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+            } else if (response.statusCode == 400) {
+                D.failure(D.errorType.AUTHENTICATION_ERROR);
+            } else if (response.statusCode != 200) {
+                D.failure(D.errorType.GENERIC_ERROR);
             } else {
-                var $ = D.htmlParse(out);
+                var $ = D.htmlParse(body);
+
                 $("tt\\:Uri").each(function(index, element) {
                     var uri = $(element).text();
                     d.resolve(uri);
                 });
-            }
+            }      
         });
         promises.push(d.promise);
     });
@@ -141,7 +151,7 @@ function healthCheck(streamUris) {
         telnetParams.command = command;
         D.device.sendTelnetCommand(telnetParams, function(out, err) {
             if (err) {
-               d.resolve("NOT OK")
+                d.resolve("NOT OK");
             } else {
                 d.resolve(out); 
             }
@@ -150,18 +160,9 @@ function healthCheck(streamUris) {
         healthPromises.push(d.promise);
     });
 
-    return D.q.all(healthPromises)
+    return D.q.all(healthPromises);
        
 }
-
-// Table for capturing status
-var table = D.createTable(
-    "Video Streaming Status",
-    [
-        { label: "Streaming status" }
-    ]
-);
-
 
 // Retrieves the capturing status and populates the Custom Driver Table
 function getCapturingStatus(data){
@@ -170,12 +171,12 @@ function getCapturingStatus(data){
         var status;
 
         if (response.indexOf("200 OK")!=-1) {
-            status = "OK"
+            status = "OK";
         } else {
             status = "NOT OK"; 
         } 
-        var profileName =  response.split("Content-Type: ")
-        var profile = profileName[0].split("/")
+        var profileName =  response.split("Content-Type: ");
+        var profile = profileName[0].split("/");
 
         if (!filterProfileName || profile[4] === filterProfileName || filterProfileName === "ALL") {
             var recordId = (index + 1) + "-" + profile[4];
@@ -184,7 +185,7 @@ function getCapturingStatus(data){
 
     });
    
-   D.success(table);
+    D.success(table);
 }
 
 function failure(err) {
