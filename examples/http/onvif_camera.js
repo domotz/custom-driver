1dccaafec9ba35e3f13b0b96f60d0493a77920b6
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-05-20 17:07:29 +0100

    CD-375 Enhancements in ONVIF Camera Monitoring Script

--- examples/http/onvif_camera.js
+++ examples/http/onvif_camera.js
@@ -33,13 +33,17 @@ var url = "/onvif/device_service";
 // filterProfileName = ["All"] to monitor all profiles.
 var filterProfileName = D.getParameter("profileName");
 
+var telnetParams = {
+    negotiationMandatory: false,
+    port: 554
+};
+
 // HTTP port parameter
 // The port number can be found in the camera's manual or through the camera's admin web app settings. Default is 80.
 var httPort = D.getParameter("port");
 
 // Generate nonce for digest authentication
 var nonce = Math.random().toString(36).substring(2,7);
-
 var currentDate = new Date();
 
 // The current timestamp in UTC format required for the Created element in the digest authentication
@@ -50,137 +54,177 @@ var created =
     (currentDate.getUTCHours() < 10 ? "0" : "") + currentDate.getUTCHours() + ":" +
     (currentDate.getUTCMinutes() < 10 ? "0" : "") + currentDate.getUTCMinutes() + ":" +
     (currentDate.getUTCSeconds() < 10 ? "0" : "") + currentDate.getUTCSeconds() + "Z";
+
 // Combine nonce, created timestamp, and password to create the string for hashing
-var combinedString =  D._unsafe.buffer.from(nonce + created + password);
+var combinedString =  nonce + created + password;
 // Hash the combined string using SHA1 and encode it in base64 for the Password Digest
-var passwordDigest = D.crypto.hash(combinedString, 'sha1', 'utf8', 'base64');
+var passwordDigest = D.crypto.hash(combinedString, 'sha1', null, 'base64');
+
 
 /**
- * Retrieves the profiles from the ONVIF camera.
- * @returns A promise that resolves with an array of profile tokens.
+ * Generates the security envelope for SOAP requests.
+ * @returns {string} The security envelope
  */
-function getProfiles() {
+function getSecurityEnvelope() {
+    return '<wsse:Security>' +
+           '<wsse:UsernameToken>' +
+           '<wsse:Username>' + username + '</wsse:Username>' +
+           '<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">' + passwordDigest + '</wsse:Password>' +
+           '<wsse:Nonce>' + D._unsafe.buffer.from(nonce).toString('base64') + '</wsse:Nonce>' +
+           '<wsse:Created>' + created + '</wsse:Created>' +
+           '</wsse:UsernameToken>' +
+           '</wsse:Security>';
+}
+
+/**
+ * Sends a SOAP request and returns a promise with the parsed response.
+ * @param {string} body  The SOAP request body.
+ * @param {function} parseResponse  A function to parse the response body.
+ * @returns A promise that resolves with the parsed response.
+ */
+function sendSoapRequest(body, parseResponse) {
     var d = D.q.defer();
     var config = {
         url: url,
         username: username,
         password: password,
         port: httPort,
-        body: '<?xml version="1.0" encoding="UTF-8"?>' +
-              '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext1.0.xsd">' +
-              '<soap:Header>' +
-              '<wsse:Security>' +
-              '<wsse:UsernameToken>' +
-              '<wsse:Username>' + username + '</wsse:Username>' +
-              '<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">' + passwordDigest + '</wsse:Password>' +
-              '<wsse:Nonce>' + nonce + '</wsse:Nonce>' +
-              '<wsse:Created>' + created + '</wsse:Created>' +
-              '</wsse:UsernameToken>' +
-              '</wsse:Security>' +
-              '</soap:Header>' +
-              '<soap:Body>' +
-              '<trt:GetProfiles/>' +
-              '</soap:Body>' +
-              '</soap:Envelope>'
+        body: body
     };
-    D.device.http.post(config, function(error, response, body){
-        if (error) {          
+    
+    D.device.http.post(config, function(error, response, body) {
+        if (error) {
             console.error(error);
-            D.failure(D.errorType.GENERIC_ERROR);
+            d.reject(D.errorType.GENERIC_ERROR);
         } else if (!response) {
-            D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+            d.reject(D.errorType.RESOURCE_UNAVAILABLE);
         } else if (response.statusCode == 400) {
-            D.failure(D.errorType.AUTHENTICATION_ERROR);
+            d.reject(D.errorType.AUTHENTICATION_ERROR);
         } else if (response.statusCode != 200) {
-            D.failure(D.errorType.GENERIC_ERROR);
+            d.reject(D.errorType.GENERIC_ERROR);
         } else {
-            var $ = D.htmlParse(body);
-            var profiles = $("trt\\:Profiles"); 
-            var profileTokens = [];
-            profiles.each(function(index, element) {
-                var token = $(element).attr("token");
-                profileTokens.push(token);
-            });
-            d.resolve(profileTokens);
+            var result = parseResponse(body);
+            d.resolve(result);
         }
     });
+
     return d.promise;
 }
 
 /**
- * Retrieves the stream URIs for each profile token
- * @param {Array} profileTokens Array of profile tokens
- * @returns A promise that resolves with an array of stream URIs
+ * Retrieves profile tokens.
+ * @returns A promise that resolves with an array of profile tokens.
  */
-function getStreamURIs(profileTokens){
-    var xmlPayload = '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl">' +
-                     '<soap:Header>' +
-                     '<wsse:Security soap:mustUnderstand="1">' +
-                     '<wsse:UsernameToken>' +
-                     '<wsse:Username>' + username + '</wsse:Username>' +
-                     '<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">' + passwordDigest + '</wsse:Password>' +
-                     '<wsse:Nonce>' + nonce + '</wsse:Nonce>' +
-                     '<wsse:Created>' + created + '</wsse:Created>' +
-                     '</wsse:UsernameToken>' +
-                     '</wsse:Security>' +
-                     '</soap:Header>' +
-                     '<soap:Body>';
-    
-    var promises = [];
-   
-    profileTokens.forEach(function(profileToken) {
-        var d = D.q.defer();
-        var streamUris = xmlPayload + '<trt:GetStreamUri>' +
-                                      '<trt:StreamSetup>' +
-                                      '<trt:Stream xmlns:trt="http://www.onvif.org/ver10/media/wsdl">RTP-Unicast</trt:Stream>' +
-                                      '<trt:Transport xmlns:trt="http://www.onvif.org/ver10/media/wsdl">' +
-                                      '<trt:Protocol>RTSP</trt:Protocol>' +
-                                      '</trt:Transport>' +
-                                      '</trt:StreamSetup>' +
-                                      '<trt:ProfileToken>' + profileToken + '</trt:ProfileToken>' +
-                                      '</trt:GetStreamUri>' +
-                                      '</soap:Body>' +
-                                      '</soap:Envelope>';
+function getProfiles() {
+    var payload = '<?xml version="1.0" encoding="UTF-8"?>' +
+                   '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext1.0.xsd">' +
+                   '<soap:Header>' + getSecurityEnvelope() + '</soap:Header>' +
+                   '<soap:Body>' +
+                   '<trt:GetProfiles/>' +
+                   '</soap:Body>' +
+                   '</soap:Envelope>';
 
-        var config = {
-            url: url,
-            username: username,
-            password: password,
-            port: httPort,
-            body: streamUris
-        };                            
-        D.device.http.post(config, function(error, response, body){
-
-            if (error) {          
-                console.error(error);
-                D.failure(D.errorType.GENERIC_ERROR);
-            } else if (!response) {
-                D.failure(D.errorType.RESOURCE_UNAVAILABLE);
-            } else if (response.statusCode == 400) {
-                D.failure(D.errorType.AUTHENTICATION_ERROR);
-            } else if (response.statusCode != 200) {
-                D.failure(D.errorType.GENERIC_ERROR);
-            } else {
-                var $ = D.htmlParse(body);
-                $("tt\\:Uri").each(function(index, element) {
-                    var uri = $(element).text();
-                    d.resolve(uri);
-                });
-            }      
+    return sendSoapRequest(payload, function(body) {
+        var $ = D.htmlParse(body);
+        var profiles = $("trt\\:Profiles");
+        var profileTokens = [];
+        profiles.each(function(index, element) {
+            var token = $(element).attr("token");
+            profileTokens.push(token);
         });
-        promises.push(d.promise);
+        return profileTokens;
     });
-    return D.q.all(promises);
 }
 
-// Encoding credentials for authorization
-var authString = username + ":" + password;
-var base64AuthString = D._unsafe.buffer.from(authString).toString('base64');
+/**
+ * Retrieves the stream URIs for each profile token.
+ * @param {Array} profileTokens - Array of profile tokens.
+ * @returns A promise that resolves with an array of stream URIs.
+ */
+function getStreamURIs(profileTokens) {
+    var startPayload = '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:trt="http://www.onvif.org/ver10/media/wsdl">' +
+                       '<soap:Header>' + getSecurityEnvelope() + '</soap:Header>' +
+                       '<soap:Body>';
+    var endPayload = '</soap:Body></soap:Envelope>';
+    var promises = profileTokens.map(function(profileToken) {
+    var payload = startPayload + '<trt:GetStreamUri>' +
+                                    '<trt:StreamSetup>' +
+                                    '<trt:Stream xmlns:trt="http://www.onvif.org/ver10/media/wsdl">RTP-Unicast</trt:Stream>' +
+                                    '<trt:Transport xmlns:trt="http://www.onvif.org/ver10/media/wsdl">' +
+                                    '<trt:Protocol>RTSP</trt:Protocol>' +
+                                    '</trt:Transport>' +
+                                    '</trt:StreamSetup>' +
+                                    '<trt:ProfileToken>' + profileToken + '</trt:ProfileToken>' +
+                                    '</trt:GetStreamUri>' + endPayload;
 
-var telnetParams = {
-    negotiationMandatory: false,
-    port: 554
-};
+        return sendSoapRequest(payload, function(body) {
+            var $ = D.htmlParse(body);
+            var uris = [];
+            $("tt\\:Uri").each(function(index, element) {
+                uris.push($(element).text());
+            });
+            return uris;
+        });
+    });
+
+    return D.q.all(promises)
+        .then(function(results) {
+            return results;
+        });
+}
+
+/**
+ * Retrieves the nonce and realm values from the WWW-Authenticate header of an RTSP DESCRIBE request.
+ * @param {string} uri The RTSP URI to send the DESCRIBE request to.
+ * @returns {Promise} A promise that resolves with an object containing the realm and nonce values.
+ */
+function getNonceAndRealm(uri) {
+    var d = D.q.defer();
+    var command = "DESCRIBE " + uri + " RTSP/1.0\r\nCSeq: 1\r\n";
+    telnetParams.command = command;
+
+    D.device.sendTelnetCommand(telnetParams, function(out, err) {
+        if (err) {
+            d.reject("Initial request failed: " + err);
+        } else {
+            var authenticateHeaderArray = out.match(/WWW-Authenticate: (.+)/);
+            if (authenticateHeaderArray && authenticateHeaderArray[1]) {
+                var authenticateHeader = authenticateHeaderArray[1];
+                var realmMatch = authenticateHeader.match(/realm="([^"]*)"/);
+                var nonceMatch = authenticateHeader.match(/nonce="([^"]*)"/);
+
+                if (realmMatch && nonceMatch) {
+                    var realm = realmMatch[1];
+                    var nonce = nonceMatch[1];
+                    d.resolve({ realm: realm, nonce: nonce });
+                } else {
+                    d.reject("realm or nonce not found in WWW-Authenticate header");
+                }
+            } else {
+                d.reject("WWW-Authenticate header not found");
+            }
+        }
+    });
+
+    return d.promise;
+}
+
+
+/**
+ * Generates the Digest Authorization header for RTSP requests.
+ * @param {string} realm The realm value from the WWW-Authenticate header.
+ * @param {string} nonce The nonce value from the WWW-Authenticate header.
+ * @param {string} uri The RTSP URI.
+ * @param {string} method The RTSP method
+ * @returns {string} The Digest Authorization header.
+ */
+function generateDigestAuth(realm, nonce, uri, method) {
+    var hash1 = D.crypto.hash(username + ":" + realm + ":" + password, "md5", "utf8", "hex");
+    var hash2 = D.crypto.hash(method + ":" + uri, "md5", "utf8", "hex");
+    var response = D.crypto.hash(hash1 + ":" + nonce + ":" + hash2, "md5", "utf8", "hex");
+    var authHeader = 'Digest username="' + username + '", realm="' + realm + '", nonce="' + nonce + '", uri="' + uri + '", response="' + response + '"';
+    return authHeader;
+}
 
 /**
  * Checks the health of the camera connection.
@@ -192,18 +236,27 @@ function healthCheck(streamUris) {
 
     streamUris.forEach(function(uri) {
         var d = D.q.defer();
-        var command = "DESCRIBE " + uri + " RTSP/1.0\r\nCSeq: 1\r\nAuthorization: Basic " + base64AuthString + "\r\n";
-        telnetParams.command = command;
-        D.device.sendTelnetCommand(telnetParams, function(out, err) {
-            if (err) {
+        getNonceAndRealm(uri)
+            .then(function(digestParams) {
+                var authHeader = generateDigestAuth(digestParams.realm, digestParams.nonce, uri, "DESCRIBE");
+                var command = "DESCRIBE " + uri + " RTSP/1.0\r\nCSeq: 2\r\nAuthorization: " + authHeader + "\r\n";
+                telnetParams.command = command;
+            
+                D.device.sendTelnetCommand(telnetParams, function(out, err) {
+                    if (err) {
+                        d.resolve("NOT OK");
+                    } else {
+                        d.resolve(out);
+                    }
+                });
+            }).catch(function(error) {
+                console.log(error);
                 d.resolve("NOT OK");
-            } else {
-                d.resolve(out); 
-            }
-        });
+            });
+
         healthPromises.push(d.promise);
     });
-    return D.q.all(healthPromises);    
+    return D.q.all(healthPromises);
 }
 
 // Retrieves the capturing status and populates the Custom Driver Table
@@ -217,12 +270,11 @@ function getCapturingStatus(data){
         } 
         var profileName =  response.split("Content-Type: ");
         var profile = profileName[0].split("/");
-        if (!filterProfileName || profile[4] === filterProfileName || filterProfileName[0].toLowerCase() === "ALL") {
+        if (!filterProfileName || profile[4] === filterProfileName || filterProfileName === "ALL") {
             var recordId = (index + 1) + "-" + profile[4];
             table.insertRecord(recordId, [status]);
         }
     });
-   
     D.success(table);
 }
 
