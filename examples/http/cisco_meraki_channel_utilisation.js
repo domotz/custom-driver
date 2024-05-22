234b57e21e42ad9ac0b00f6af591d79d3caa4de1
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-05-22 21:58:02 +0100

    CD-160 - Add Cisco meraki Channe Utilization script - Add script description JSON

---
+++ examples/http/cisco_meraki_channel_utilisation.js
@@ -0,0 +1,211 @@
+/**
+ * Domotz Custom Driver 
+ * Name: Cisco Meraki Channel Utilisation
+ * Description: This script extracts channel utilization information from Cisco Meraki networks using the Meraki Dashboard API.
+ * 
+ * Communication protocol is HTTPS
+ * 
+ * Tested on Cisco Meraki version wireless-25-13 
+ *
+ * Creates a Custom Driver table with the following columns:
+ *      - Channel Utilization: Percentage of total channel utiliation for the given radio
+ *      - Wifi Utilization: Percentage of wifi channel utiliation for the given radio
+ *      - Non Wifi Utilization: Percentage of non-wifi channel utiliation for the given radio
+ *      - Start Timestamp: The start time of the channel utilization interval
+ *      - End Timestamp: The end time of the channel utilization interval
+ * 
+ **/
+
+var device = D.createExternalDevice("api.meraki.com"); 
+
+// The Id of the organization, obtained from the Cisco Meraki dashboard
+var organizationId = D.getParameter("organizationID"); 
+
+// If networkId is 'ALL', it returns all network IDs for the given organization.
+// Otherwise, it resolves with the specified networkId
+var networkId = D.getParameter("networkID"); // The ID of the network
+
+// Table to store channel utilization data 
+var table = D.createTable(
+    "Channel Utilization",
+    [
+        { label: "Channel Utilization", unit: "%", valueType: D.valueType.NUMBER },
+        { label: "Wifi Utilization",  unit: "%", valueType: D.valueType.NUMBER },
+        { label: "Non Wifi Utilization",  unit: "%", valueType: D.valueType.NUMBER },
+        { label: "Start Timestamp", valueType: D.valueType.DATETIME },
+        { label: "End Timestamp", valueType: D.valueType.DATETIME }
+    ]
+);
+
+/**
+ * Function to retrieve network IDs.
+ * @returns {Promise} A promise that resolves to an array of network IDs
+ */
+function getNetworkId() {
+    var d = D.q.defer();
+    if (networkId === "ALL") {
+        var config = {
+            url: "/api/v1/organizations/" + organizationId + "/networks",
+            protocol: "https",
+            headers: {
+                "Authorization": "Bearer " + D.device.password(),
+                "Content-Type": "application/json"
+            }
+        };
+        device.http.get(config, function(error, response, body){   
+            if (error) {
+                console.error(error);
+                D.failure(D.errorType.GENERIC_ERROR);  
+            } else if (response.statusCode == 404) {
+                D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+            } else if (response.statusCode === 401) {
+                console.error("Invalid API key");
+                D.failure(D.errorType.AUTHENTICATION_ERROR);
+            } else if (response.statusCode != 200) {
+                console.error(body);
+                D.failure(D.errorType.GENERIC_ERROR);
+            } else {
+                var data = JSON.parse(body);
+                var networkIDs = [];
+                for (var i = 0; i < data.length; i++) {
+                    networkIDs.push(data[i].id);
+                }
+                d.resolve(networkIDs);
+            }
+        });
+    } else {
+        d.resolve([networkId]);
+    }
+    return d.promise;
+}
+
+/**
+ * Function to retrieve channel utilzation info for given network IDs
+ * @param {Array} networkIDS An array of network IDs
+ * @returns {Promise} A promise that resolves to an array of channel utilzation data 
+ */
+function getChannelUtilization(networkIDs) {
+    var promises = networkIDs.map(function(id) {
+        var d = D.q.defer();
+        var config = {
+            url: "/api/v1/networks/" + id + "/networkHealth/channelUtilization",
+            protocol: "https",
+            headers: {
+                "Authorization": "Bearer " + D.device.password(),
+                "Content-Type": "application/json"
+            }
+        };
+        device.http.get(config, function(error, response, body){   
+            if (error) {
+                console.error(error);
+                D.failure(D.errorType.GENERIC_ERROR);  
+            } else if (response.statusCode == 404) {
+                D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+            } else if (response.statusCode === 401) {
+                console.error("Invalid API key");
+                D.failure(D.errorType.AUTHENTICATION_ERROR);
+            } else if (response.statusCode === 400) {
+                console.error("Channel utilization data not available for network: " + id + ". Only wireless networks are supported.");        
+                d.resolve(null);
+            } else if (response.statusCode != 200) {
+                D.failure(D.errorType.GENERIC_ERROR);
+            } else {
+                d.resolve(body);
+            }
+        });
+        return d.promise;
+    });
+    return D.q.all(promises)
+        .then(function(results) {
+            return results.filter(function(result) { return result !== null; });
+        });
+}
+
+function sanitize(output){
+    var recordIdReservedWords = ['\\?', '\\*', '\\%', 'table', 'column', 'history'];
+    var recordIdSanitisationRegex = new RegExp(recordIdReservedWords.join('|'), 'g');
+    return output.replace(recordIdSanitisationRegex, '').slice(0, 50).replace(/\s+/g, '-').toLowerCase();
+}
+
+function formatTimestamp(timestamp) {
+    var date = new Date(timestamp);
+    var formattedDate =
+                (date.getUTCMonth() + 1 < 10 ? "0" : "") + (date.getUTCMonth() + 1) + "/" +
+                (date.getUTCDate() < 10 ? "0" : "") + date.getUTCDate() + "/" +
+                date.getUTCFullYear() + " " +
+                (date.getUTCHours() < 10 ? "0" : "") + date.getUTCHours() + ":" +
+                (date.getUTCMinutes() < 10 ? "0" : "") + date.getUTCMinutes() + ":" +
+                (date.getUTCSeconds() < 10 ? "0" : "") + date.getUTCSeconds();
+    return formattedDate;
+}
+
+// Function to extract data from the response body and populates custom table
+function extractData(data) {
+    data.forEach(function (devices) {      
+        var device = JSON.parse(devices);  
+        device.forEach(function (deviceData){
+            for (var key in deviceData) {
+                var serial = deviceData.serial;
+                if (Array.isArray(deviceData[key])) {
+                    deviceData[key].forEach(function(wifi, index) {
+                        var channelUtilization = wifi.utilization ? wifi.utilization : "N/A";
+                        var wifiUtilization = wifi.wifi ? wifi.wifi : "N/A";
+                        var nonWifiUtilization = wifi.non_wifi ? wifi.non_wifi : "N/A";
+                        var startTimestamp = wifi.start_ts ? wifi.start_ts : "N/A";
+                        var endTimestamp = wifi.end_ts ? wifi.end_ts : "N/A";
+                        var recordId = (index + 1) + "-" + serial + "-"  + key ;
+                        table.insertRecord(sanitize(recordId), [ 
+                            channelUtilization,
+                            wifiUtilization,
+                            nonWifiUtilization,
+                            formatTimestamp(startTimestamp),
+                            formatTimestamp(endTimestamp)
+                        ]);
+
+                    });
+                }
+            }
+            
+        }); 
+    });
+    D.success(table);
+
+}
+
+/**
+ * @remote_procedure
+ * @label Validate Cisco Meraki Channel Utilization
+ * @documentation This procedure is used to validate the ability to retrieve channel utilization info from Cisco Meraki networks
+ */
+function validate(){
+    getNetworkId()
+        .then(getChannelUtilization)
+        .then(function (response) {
+            if (response && response.length > 0) {
+                console.log("Validation successful");
+                D.success();
+            } else {
+                console.error("Validation failed");
+                D.failure(D.errorType.PARSING_ERROR);
+            }
+        })
+        .catch(function (err) {
+            console.error(err);
+            D.failure(D.errorType.GENERIC_ERROR);
+        });
+}
+
+/**
+ * @remote_procedure
+ * @label Get Channel Utilization data 
+ * @documentation This procedure is used to retrieve channel utilization information from Cisco Meraki networks.
+ */
+function get_status() {
+    getNetworkId()
+        .then(getChannelUtilization)
+        .then(extractData)
+        .catch(function (err) {
+            console.error(err);
+            D.failure(D.errorType.GENERIC_ERROR);
+        });
+}
