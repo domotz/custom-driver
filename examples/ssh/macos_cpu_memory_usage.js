48a01c7d02bd1d45cb02b751525a43eedd0d3a69
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-07-16 14:21:00 +0100

    CD-397

--- examples/ssh/macos_cpu_memory_usage.js
+++ examples/ssh/macos_cpu_memory_usage.js
@@ -21,7 +21,7 @@
  **/
 
 var cmdCpuMemoryInfo = 'system_profiler SPHardwareDataType -json'
-var cmdcpuAndMemoryUsage  = 'memory_pressure && sysctl -n hw.logicalcpu vm.loadavg'
+var cmdcpuAndMemoryUsage  = 'memory_pressure && sysctl  hw.logicalcpu vm.loadavg'
 
 var sshConfig = {
   'username': D.device.username(),
@@ -97,36 +97,38 @@ function extractVariables (output) {
   // Parsing JSON output for system_profiler command
   var cpuMemoryInfo = JSON.parse(output[0])
   if (cpuMemoryInfo && cpuMemoryInfo.SPHardwareDataType && cpuMemoryInfo.SPHardwareDataType.length > 0) {
-    var totalMemory = (cpuMemoryInfo.SPHardwareDataType[0].physical_memory).replace(/[^\d,.]/g, "") || 0
-    var cpuName = cpuMemoryInfo.SPHardwareDataType[0].cpu_type || 'N/A'
-    var processorSpeed = (cpuMemoryInfo.SPHardwareDataType[0].current_processor_speed).replace(/[^\d,.]/g, "") || 0
-    var physicalCPUPackages = cpuMemoryInfo.SPHardwareDataType[0].packages || 0
-    var coresNumber = cpuMemoryInfo.SPHardwareDataType[0].number_processors || 0
+    var hardwareData = cpuMemoryInfo.SPHardwareDataType[0]
+    var totalMemory = hardwareData.physical_memory ? hardwareData.physical_memory.replace(/[^\d,.]/g, "") : 0;
+    var cpuName = hardwareData.cpu_type || 'N/A'
+    var processorSpeed = hardwareData.current_processor_speed ? hardwareData.current_processor_speed.replace(/[^\d,.]/g, "") : 0;
+    var physicalCPUPackages = hardwareData.packages || 0
+    var coresNumber = hardwareData.number_processors || 0
   } else {
     console.error('Invalid or empty SPHardwareDataType')
   }
   // Parsing output from memory_pressure and sysctl commands
   if (output[1]) {
     var cpuMemoryUsage = output[1].trim().split('\n').filter(function(line) { return line.trim() !== '' })
-    var pagesFree = parseInt(cpuMemoryUsage[2].split(': ')[1].trim()) || 0
-    var pageSize = parseInt(cpuMemoryUsage[0].match(/page size of (\d+)/)[1].trim()) || 0
-    var availableMemoryGB = bytesToGB(pagesFree * pageSize)
-    var logicalCPUNumber = cpuMemoryUsage.slice(-2)[0] || 0
-    var loadAvg = cpuMemoryUsage.slice(-2)[1] || 0
-    var loadAverage1min = loadAvg ? loadAvg.replace(/[{}]/g, '').trim().split(' ')[0] : 0
+    var memoryFreePercentage = (output[1].match(/System-wide memory free percentage: (\d+)%/)[1]) 
+    var availableMemoryGB = (totalMemory * memoryFreePercentage) / 100
+    var logicalCPUNumber = cpuMemoryUsage.find(function (line) { return line.indexOf('hw.logicalcpu') !== -1});
+    logicalCPUNumber = logicalCPUNumber ? parseInt(logicalCPUNumber.split(':')[1].trim()) : 0;
+    var loadAvg = cpuMemoryUsage.find(function (line) { return line.indexOf('vm.loadavg') !== -1})
+    loadAvg = loadAvg ? loadAvg.split(':')[1].trim() : 0;
+    var loadAverage1min = loadAvg ? loadAvg.replace(/[{}]/g, '').trim().split(' ')[0] : 0;
   } else {
     D.failure(D.errorType.PARSING_ERROR)
   }
   var variables = [
     D.createVariable('total-memory', 'Total Memory', totalMemory, 'GB', D.valueType.NUMBER ),
     D.createVariable('available-memory', 'Available Memory', availableMemoryGB, 'GB', D.valueType.NUMBER ),
-    D.createVariable('memory-usage', 'Memory Usage', (((totalMemory - availableMemoryGB) / totalMemory) * 100).toFixed(2), '%', D.valueType.NUMBER ),
+    D.createVariable('memory-usage', 'Memory Usage', totalMemory > 0 ? (((totalMemory - availableMemoryGB) / totalMemory) * 100).toFixed(2) : 0, '%', D.valueType.NUMBER ),
     D.createVariable('cpu-name', 'CPU Name', cpuName, null, D.valueType.STRING),
     D.createVariable('processor-speed', 'Processor Speed', processorSpeed, 'GHz', D.valueType.NUMBER),
     D.createVariable('physical-processors-packages', 'Physical Processors Packages', physicalCPUPackages, null, D.valueType.NUMBER),
     D.createVariable('logical-processors-number', 'Logical Processors Number', logicalCPUNumber, null, D.valueType.NUMBER),
     D.createVariable('cores-number', 'Cores Number', coresNumber, null, D.valueType.NUMBER),
-    D.createVariable('cpu-average', 'CPU Load Average (1min)', (loadAverage1min / coresNumber) * 100, '%', D.valueType.NUMBER)
+    D.createVariable('cpu-average', 'CPU Load Average (1min)', coresNumber > 0 ? (loadAverage1min / coresNumber) * 100 : 0, '%', D.valueType.NUMBER)
   ]
   D.success(variables)
 }
