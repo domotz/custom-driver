a85dcbefe4cde7410af3f83f7cd45aaf6967e315
Firas <firas.aloulou@pivasoftware.com>
2024-08-13 14:54:29 +0100

    CD-422 - Delete Port Key column

--- examples/http/esxi_network_virtual_nics_configuration.js
+++ examples/http/esxi_network_virtual_nics_configuration.js
@@ -16,7 +16,6 @@
  * - IP Address
  * - Subnet Mask
  * - MAC Address
- * - Port Key
  *
  **/
 
@@ -25,18 +24,17 @@ const url = '/sdk'
 
 
 // Creation of Network Virtual NIC Configuration table
-const networkVirtualNicConfigurationTable = D.createTable(
+var networkVirtualNicConfigurationTable = D.createTable(
     'Network Virtual NIC Configuration',
     [
-        {label: 'Device', valueType: D.valueType.STRING},
-        {label: 'Port Group', valueType: D.valueType.STRING},
-        {label: 'DHCP Enabled', valueType: D.valueType.STRING},
-        {label: 'IP Address', valueType: D.valueType.STRING},
-        {label: 'Subnet Mask', valueType: D.valueType.STRING},
-        {label: 'MAC Address', valueType: D.valueType.STRING},
-        {label: 'Port Key', valueType: D.valueType.STRING}
+        { label: 'Device', valueType: D.valueType.STRING },
+        { label: 'Port Group', valueType: D.valueType.STRING },
+        { label: 'DHCP Enabled', valueType: D.valueType.STRING },
+        { label: 'IP Address', valueType: D.valueType.STRING },
+        { label: 'Subnet Mask', valueType: D.valueType.STRING },
+        { label: 'MAC Address', valueType: D.valueType.STRING }
     ]
-);
+)
 
 /**
  * Sends a SOAP request and returns a promise with the parsed response.
@@ -45,32 +43,32 @@ const networkVirtualNicConfigurationTable = D.createTable(
  * @returns A promise that resolves with the parsed response.
  */
 function sendSoapRequest (body, extractData) {
-  const d = D.q.defer()
+    const d = D.q.defer()
 
-  let config ={
-    url,
-    protocol: "https",
-    rejectUnauthorized: false,
-    body,
-    jar: true
-  }
-
-  D.device.http.post(config, function (error, response, body) {
-    if (error) {
-      console.error(error)
-      D.failure(D.errorType.GENERIC_ERROR)
-    } else if (!response) {
-      D.failure(D.errorType.RESOURCE_UNAVAILABLE)
-    } else if (response.statusCode === 400) {
-      D.failure(D.errorType.AUTHENTICATION_ERROR)
-    } else if (response.statusCode !== 200) {
-      D.failure(D.errorType.GENERIC_ERROR)
-    } else {
-      const result = extractData(body)
-      d.resolve(result)
+    let config ={
+        url,
+        protocol: "https",
+        rejectUnauthorized: false,
+        body,
+        jar: true
     }
-  })
-  return d.promise
+
+    D.device.http.post(config, function (error, response, body) {
+        if (error) {
+            console.error(error)
+            D.failure(D.errorType.GENERIC_ERROR)
+        } else if (!response) {
+            D.failure(D.errorType.RESOURCE_UNAVAILABLE)
+        } else if (response.statusCode === 400) {
+            D.failure(D.errorType.AUTHENTICATION_ERROR)
+        } else if (response.statusCode !== 200) {
+            D.failure(D.errorType.GENERIC_ERROR)
+        } else {
+            const result = extractData(body)
+            d.resolve(result)
+        }
+    })
+    return d.promise
 }
 
 /**
@@ -79,9 +77,9 @@ function sendSoapRequest (body, extractData) {
  * @returns {string} The complete SOAP payload as a string, including the envelope and body.
  */
 function createSoapPayload (soapBody) {
-  return '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:vim25="urn:vim25"><soapenv:Body>'
-      + soapBody +
-      '</soapenv:Body></soapenv:Envelope>'
+    return '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:vim25="urn:vim25"><soapenv:Body>'
+        + soapBody +
+        '</soapenv:Body></soapenv:Envelope>'
 }
 
 /**
@@ -90,8 +88,8 @@ function createSoapPayload (soapBody) {
  * @returns {string} The Session Key extracted from the SOAP response.
  */
 function getSessionKey(soapResponse) {
-  const $ = D.htmlParse(soapResponse)
-  return $('returnval').find('key').first().text()
+    const $ = D.htmlParse(soapResponse)
+    return $('returnval').find('key').first().text()
 }
 
 /**
@@ -99,15 +97,15 @@ function getSessionKey(soapResponse) {
  * @returns {Promise} A promise that resolves with the response of the login request.
  */
 function login () {
-  const payload = createSoapPayload(
-      '<vim25:Login>' +
-      '   <_this type="SessionManager">ha-sessionmgr</_this>' +
-      '   <userName>' + D.device.username() + '</userName>' +
-      '   <password>' + D.device.password() + '</password>' +
-      '</vim25:Login>'
-  )
+    const payload = createSoapPayload(
+        '<vim25:Login>' +
+        '   <_this type="SessionManager">ha-sessionmgr</_this>' +
+        '   <userName>' + D.device.username() + '</userName>' +
+        '   <password>' + D.device.password() + '</password>' +
+        '</vim25:Login>'
+    )
     // Send the SOAP request and handle the response to extract the Session Key.
-  return sendSoapRequest(payload, getSessionKey)
+    return sendSoapRequest(payload, getSessionKey)
 }
 
 /**
@@ -116,8 +114,8 @@ function login () {
  * @returns {string} The container ID extracted from the SOAP response.
  */
 function getContainerIdFromSoap(soapResponse) {
-  const $ = D.htmlParse(soapResponse)
-  return $('returnval').text();
+    const $ = D.htmlParse(soapResponse)
+    return $('returnval').text();
 }
 
 /**
@@ -125,16 +123,16 @@ function getContainerIdFromSoap(soapResponse) {
  * @returns {Promise} A promise that resolves with the container ID from the SOAP response.
  */
 function createAllHostContainer() {
-  const payload = createSoapPayload(
-      '<CreateContainerView xmlns="urn:vim25">' +
-      '    <_this type="ViewManager">ViewManager</_this>' +
-      '    <container type="Folder">ha-folder-root</container>' +
-      '    <type>HostSystem</type>' +
-      '    <recursive>true</recursive>' +
-      '</CreateContainerView>'
-  )
-  // Send the SOAP request and extract the container ID from the response.
-  return sendSoapRequest(payload, getContainerIdFromSoap)
+    const payload = createSoapPayload(
+        '<CreateContainerView xmlns="urn:vim25">' +
+        '    <_this type="ViewManager">ViewManager</_this>' +
+        '    <container type="Folder">ha-folder-root</container>' +
+        '    <type>HostSystem</type>' +
+        '    <recursive>true</recursive>' +
+        '</CreateContainerView>'
+    )
+    // Send the SOAP request and extract the container ID from the response.
+    return sendSoapRequest(payload, getContainerIdFromSoap)
 }
 
 /**
@@ -143,8 +141,8 @@ function createAllHostContainer() {
  * @returns {string} The host reference extracted from the SOAP response.
  */
 function getHostRefFromSoap(soapResponse) {
-  const $ = D.htmlParse(soapResponse)
-  return $('returnval').find('ManagedObjectReference').first().text()
+    const $ = D.htmlParse(soapResponse)
+    return $('returnval').find('ManagedObjectReference').first().text()
 }
 
 /**
@@ -153,14 +151,14 @@ function getHostRefFromSoap(soapResponse) {
  * @returns {Promise} A promise that resolves with the host reference extracted from the SOAP response.
  */
 function fetchContainer(containerId) {
-  const payload = createSoapPayload(
-      '<Fetch xmlns="urn:vim25">' +
-      '    <_this type="ContainerView">' + containerId + '</_this>' +
-      '    <prop>view</prop>' +
-      '</Fetch>'
-  )
-  // Send the SOAP request and extract the host reference from the response.
-  return sendSoapRequest(payload, getHostRefFromSoap)
+    const payload = createSoapPayload(
+        '<Fetch xmlns="urn:vim25">' +
+        '    <_this type="ContainerView">' + containerId + '</_this>' +
+        '    <prop>view</prop>' +
+        '</Fetch>'
+    )
+    // Send the SOAP request and extract the host reference from the response.
+    return sendSoapRequest(payload, getHostRefFromSoap)
 }
 
 /**
@@ -169,22 +167,22 @@ function fetchContainer(containerId) {
  * @returns {Promise} A promise that resolves with the properties of the host as extracted from the SOAP response.
  */
 function retrieveProprieties(hostRef) {
-  const payload = createSoapPayload(
-      '<RetrieveProperties xmlns="urn:vim25">' +
-      '    <_this type="PropertyCollector">ha-property-collector</_this>' +
-      '    <specSet>' +
-      '        <propSet>' +
-      '            <type>HostSystem</type>' +
-      '            <pathSet>config.network.vnic</pathSet>' +
-      '        </propSet>' +
-      '        <objectSet>' +
-      '            <obj type="HostSystem">' + hostRef + '</obj>' +
-      '        </objectSet>' +
-      '    </specSet>' +
-      '</RetrieveProperties>'
-  )
-  // Send the SOAP request and handle the response to generate the desired variables.
-  return sendSoapRequest(payload, generateTableOutput)
+    const payload = createSoapPayload(
+        '<RetrieveProperties xmlns="urn:vim25">' +
+        '    <_this type="PropertyCollector">ha-property-collector</_this>' +
+        '    <specSet>' +
+        '        <propSet>' +
+        '            <type>HostSystem</type>' +
+        '            <pathSet>config.network.vnic</pathSet>' +
+        '        </propSet>' +
+        '        <objectSet>' +
+        '            <obj type="HostSystem">' + hostRef + '</obj>' +
+        '        </objectSet>' +
+        '    </specSet>' +
+        '</RetrieveProperties>'
+    )
+    // Send the SOAP request and handle the response to generate the desired variables.
+    return sendSoapRequest(payload, generateTableOutput)
 }
 
 /**
@@ -197,8 +195,7 @@ function populateTable(vNicDetails) {
         vNicDetails.dhcpEnabled,
         vNicDetails.ipAddress,
         vNicDetails.subnetMask,
-        vNicDetails.macAddress,
-        vNicDetails.portKey
+        vNicDetails.macAddress
     ]);
 }
 
@@ -222,8 +219,7 @@ function generateTableOutput(soapResponse) {
             dhcpEnabled: ip.find('dhcp').text() === 'true' ? 'Yes' : 'No',
             ipAddress: ip.find('ipAddress').text() || "N/A",
             subnetMask: ip.find('subnetMask').text() || "N/A",
-            macAddress: spec.find('mac').text() || "N/A",
-            portKey: vnicInfo.find('port').text() || "N/A"
+            macAddress: spec.find('mac').text() || "N/A"
         });
     });
 
@@ -236,12 +232,12 @@ function generateTableOutput(soapResponse) {
  * @documentation This procedure is used to validate if the driver can be applied on a device during association as well as validate any credentials provided
  */
 function validate () {
-  login()
-  .then(function (sessionKey) {
-        if(sessionKey && sessionKey.length > 0){
-          D.success();
-        }
-    })
+    login()
+        .then(function (sessionKey) {
+            if(sessionKey && sessionKey.length > 0){
+                D.success();
+            }
+        })
         .catch(function () {
             D.failure(D.errorType.GENERIC_ERROR);
         });
@@ -253,13 +249,13 @@ function validate () {
  * @documentation This procedure retrieves the ESXi host Network Virtual NIC Configuration
  */
 function get_status() {
-  login().
-  then(createAllHostContainer).
-  then(fetchContainer).
-  then(retrieveProprieties).
-  then(D.success)
-      .catch(function (err) {
-        console.error(err);
-        D.failure(D.errorType.GENERIC_ERROR);
-      });
+    login().
+    then(createAllHostContainer).
+    then(fetchContainer).
+    then(retrieveProprieties).
+    then(D.success)
+        .catch(function (err) {
+            console.error(err);
+            D.failure(D.errorType.GENERIC_ERROR);
+        });
 }
