bcaebe8d5ad32795c00a461932260f3bfaae0e2f
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-08-27 15:19:45 +0100

    CD-425 - Add additional fields to Dante Director device status monitoring script

--- examples/http/dante_director_device_status_monitoring.js
+++ examples/http/dante_director_device_status_monitoring.js
@@ -2,18 +2,30 @@
  * Domotz Custom Driver 
  * Name: Dante Director - Device Status Monitoring
  * Description: This script integrates with the Dante Managed API to fetch and monitor device statuses
- * It retrieves detailed information about each device, including connectivity status, subscription status and channel information
+ * It retrieves detailed information about each device, including connectivity, clocking, latency, and subscription status,
+ * as well as detailed channel information
  * 
  * Communication protocol is HTTPS
  * 
  * Tested with Dante Director using the Dante Managed API 
  *
   * Creates a Custom Driver Table with the following columns:
- *      - Name: The name of the device.
+ *      - Name: The name of the device
+ *      - Manufacturer Name: The name of the manufacturer of the device
+ *      - Product Model: The model name of the device
+ *      - Dante Version: The version of Dante software running on the device 
+ *      - Address: The network address of the device
+ *      - Status ID: Identifier representing the current status of the device.
+ *      - Clocking Status: Indicates the clocking status of the device
+ *      - Latency Status: The latency status for the device.
  *      - Connectivity Status: The current connectivity status of the device
  *      - Subscription Status: The subscription status of the device
  *      - RX Channels: List of RX channels associated with the device
  *      - TX Channels: List of TX channels associated with the device
+ *      - Alert Message Clocking: Alert message related to clocking issues
+ *      - Alert Message Connectivity: Alert message related to connectivity issues
+ *      - Alert Message Latency: Alert message related to latency issues
+ *      - Alert Message Subscriptions: Alert message related to subscription issues
  * 
  **/
 
@@ -32,10 +44,21 @@ var danteHost = 'api.director.dante.cloud'
 var table = D.createTable(
   'Devices', [
     {label: 'Name', valueType: D.valueType.STRING },
+    {label: 'Manufacturer Name', valueType: D.valueType.STRING },
+    {label: 'Product Model', valueType: D.valueType.STRING },
+    {label: 'Dante Version', valueType: D.valueType.STRING },
+    {label: 'Address', valueType: D.valueType.STRING },
+    {label: 'Status ID', valueType: D.valueType.STRING },
+    {label: 'Clocking Status', valueType: D.valueType.STRING },
+    {label: 'Latency Status', valueType: D.valueType.STRING },
     {label: 'Connectivity Status', valueType: D.valueType.STRING },
     {label: 'Subscription Status', valueType: D.valueType.STRING },
     {label: 'RX Channels', valueType: D.valueType.STRING },
-    {label: 'TX Channels', valueType: D.valueType.STRING }
+    {label: 'TX Channels', valueType: D.valueType.STRING },
+    {label: 'Alert Message Clocking', valueType: D.valueType.STRING },
+    {label: 'Alert Message Connectivity', valueType: D.valueType.STRING },
+    {label: 'Alert Message Latency', valueType: D.valueType.STRING },
+    {label: 'Alert Message Subscriptions', valueType: D.valueType.STRING }
   ]
 )
 
@@ -56,13 +79,15 @@ function getDevicesInfo() {
     body: JSON.stringify({
       query: 'query Devices($domainId: ID!) { ' +
              'domain(id: $domainId) { ' +
-             'devices { id name status { connectivity subscriptions } ' +
+             'devices { id name identity { manufacturerName productModelName danteVersion } interfaces { address } status { id clocking latency connectivity subscriptions alertMessage { clocking connectivity latency subscriptions }}' +
              'rxChannels { id index name subscribedDevice subscribedChannel status } ' +
              'txChannels { id index name } ' +
              '} } }',
       variables: { domainId: domainID }
     })
   }
+
+  
   device.http.post(config, function (error, response, body) {
     if (error) {
       console.error(error)
@@ -101,21 +126,45 @@ function extractData(data) {
     var devices = data.devices
     for (var i = 0; i < devices.length; i++) {
       var device = devices[i]
+      var manufacturerName = device.identity && device.identity.manufacturerName ? device.identity.manufacturerName: 'N/A'
+      var productModelName = device.identity && device.identity.productModelName ? device.identity.productModelName: 'N/A'
+      var danteVersion = device.identity && device.identity.danteVersion ? device.identity.danteVersion: 'N/A'
+      var address = (device.interfaces && Array.isArray(device.interfaces) && device.interfaces.length > 0)
+      ? device.interfaces[0].address
+      : 'N/A'
+      var statusId = device.status && device.status.id ? device.status.id : 'N/A'
+      var clockingStatus = device.status && device.status.clocking ? device.status.clocking : 'N/A'
+      var latencyStatus = device.status && device.status.latency ? device.status.latency : 'N/A'
+      var connectivityStatus = device.status && device.status.connectivity ? device.status.connectivity : 'N/A'
+      var subscriptionStatus = device.status && device.status.subscriptions ? device.status.subscriptions : 'N/A'
       var rxChannels = (device.rxChannels && Array.isArray(device.rxChannels) && device.rxChannels.length > 0)
         ? device.rxChannels.map(function(channel) { return channel.name + " (" + (channel.status || 'N/A') + ")" }).join(", ")
         : 'N/A'
       var txChannels = (device.txChannels && Array.isArray(device.txChannels) && device.txChannels.length > 0)
         ? device.txChannels.map(function(channel) { return channel.name }).join(", ")
         : 'N/A'
-      var connectivityStatus = device.status && device.status.connectivity ? device.status.connectivity : 'N/A'
-      var subscriptionStatus = device.status && device.status.subscriptions ? device.status.subscriptions : 'N/A'
+      var alertMessageClocking = device.status && device.status.alertMessage && device.status.alertMessage.clocking ? device.status.alertMessage.clocking : 'N/A'
+      var alertMessageConnectivity = device.status && device.status.alertMessage && device.status.alertMessage.connectivity ? device.status.alertMessage.connectivity : 'N/A'
+      var alertMessageLatency = device.status && device.status.alertMessage && device.status.alertMessage.latency ? device.status.alertMessage.latency : 'N/A'
+      var alertMessageSubscriptions = device.status && device.status.alertMessage && device.status.alertMessage.subscriptions ? device.status.alertMessage.subscriptions : 'N/A'
       populateTable({
-        id: device.id || 'N/A',
+        id: device.id,
         name: device.name || 'N/A',
+        manufacturerName: manufacturerName,
+        productModelName: productModelName,
+        danteVersion: danteVersion,
+        address: address,
+        statusId: statusId,
+        clockingStatus: clockingStatus,
+        latencyStatus: latencyStatus,
         connectivityStatus: connectivityStatus,
-        subscriptionStatus: subscriptionStatus,
+        subscriptionStatus: subscriptionStatus,      
         rxChannels: rxChannels,
-        txChannels: txChannels
+        txChannels: txChannels,
+        alertMessageClocking: alertMessageClocking,
+        alertMessageConnectivity: alertMessageConnectivity,
+        alertMessageLatency: alertMessageLatency,
+        alertMessageSubscriptions: alertMessageSubscriptions,
       })
     }
     D.success(table)
@@ -139,10 +188,21 @@ function populateTable(devicesInfo) {
   table.insertRecord(
     sanitize(devicesInfo.id), [
       devicesInfo.name,
+      devicesInfo.manufacturerName,
+      devicesInfo.productModelName,
+      devicesInfo.danteVersion,
+      devicesInfo.address,
+      devicesInfo.statusId,
+      devicesInfo.clockingStatus,
+      devicesInfo.latencyStatus,
       devicesInfo.connectivityStatus,
       devicesInfo.subscriptionStatus,
       devicesInfo.rxChannels,
-      devicesInfo.txChannels
+      devicesInfo.txChannels,
+      devicesInfo.alertMessageClocking,
+      devicesInfo.alertMessageConnectivity,
+      devicesInfo.alertMessageLatency,
+      devicesInfo.alertMessageSubscriptions,
     ]
   )
 }
