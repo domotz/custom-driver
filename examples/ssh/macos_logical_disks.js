7fd02a613c8d8ad4cf5d9e67a009d053af04d4ab
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-09-18 18:00:24 +0100

    CD-402 - Update the Free Space and Size columns to use variables

--- examples/ssh/macos_logical_disks.js
+++ examples/ssh/macos_logical_disks.js
@@ -2,23 +2,25 @@
  * Domotz Custom Driver
  * Name: macOS - Logical Disks
  * Description: Monitors the status of logical disks within a Macintosh machine
- *
+ * 
  * Communication protocol is SSH
  *
  * Tested on macOS Version 14.5
- *
+ * 
+ * Creates Custom Driver variables:
+ *   - Free Space: The amount of free space available on the disk
+ *   - Size: The total size of the disk
+ * 
  * Creates a custom driver table with the following columns: 
  *   - Name: Volume Name of the disk
  *   - Node: Device Node (path) of the disk
  *   - Parent Disk: The disk that this volume is part of
  *   - Mount Point: The path where the volume is mounted
  *   - Usage: The percentage of the volume total space that is currently used
- *   - Free Space: The total amount of space available on the disk in gigabytes 
- *   - Size: The total size of the disk in gigabytes
  *   - File System: File system type used by the volume
  *   - ReadOnly: Indicates if the volume is read-only
  *   - Status: SMART status of the disk
- *
+ * 
  **/
 
 // SSH command to retrieve retrieve logical disks information
@@ -39,8 +41,6 @@ var table = D.createTable(
     { label: "Parent Disk", valueType: D.valueType.STRING },
     { label: "Mount Point", valueType: D.valueType.STRING },
     { label: "Usage", unit: "%", valueType: D.valueType.NUMBER },
-    { label: "Free Space", unit: "GB", valueType: D.valueType.NUMBER },
-    { label: "Size", unit: "GB", valueType: D.valueType.NUMBER },
     { label: "File System", valueType: D.valueType.STRING },
     { label: "ReadOnly", valueType: D.valueType.STRING },
     { label: "Status", valueType: D.valueType.STRING }
@@ -113,9 +113,10 @@ function parseOutput(output) {
       logicalDisks.push(currentDisk)
     }
     var filteredDisks = logicalDisks.filter(function(disk) {
-      return disk['Volume Name'] != 'Not applicable (no file system)' && disk['Mount Point'] 
+      return disk['Mount Point'] && disk['Mount Point'].includes('/Volumes')
     })
     return filteredDisks
+
   } else {
     console.error('No data found')
     D.failure(D.errorType.PARSING_ERROR)
@@ -128,49 +129,66 @@ function sanitize (output) {
   return output.replace(recordIdSanitizationRegex, '').slice(0, 50).replace(/\s+/g, '-').toLowerCase()
 }
 
-var table = D.createTable(
-  "Logical Disks", 
-  [
-    { label: "Name", valueType: D.valueType.STRING },
-    { label: "Node", valueType: D.valueType.STRING },
-    { label: "Parent Disk", valueType: D.valueType.STRING },
-    { label: "Mount Point", valueType: D.valueType.STRING },
-    { label: "Usage", unit: "%", valueType: D.valueType.NUMBER },
-    { label: "Free Space", unit: "GB", valueType: D.valueType.NUMBER },
-    { label: "Size", unit: "GB", valueType: D.valueType.NUMBER },
-    { label: "File System", valueType: D.valueType.STRING },
-    { label: "ReadOnly", valueType: D.valueType.STRING },
-    { label: "Status", valueType: D.valueType.STRING }
-  ]
-)
+function convertToAppropriateUnits(bytes) {
+  if (bytes >= 1e9) {
+    return {
+      value: (bytes / 1e9).toFixed(2),
+      unit: 'GB'
+    }
+  } else {
+    return {
+      value: (bytes / 1e6).toFixed(2),
+      unit: 'MB'
+    }
+  }
+}
+
 /**
  * Extracts relevant variables from parsed logical disks and populates the table
  * @param {Array} logicalDisks Array of objects representing parsed logical disk information
  */
 function populateTable(logicalDisks) {
+  var parentDiskValues = {}
+  var variables = []
   logicalDisks.forEach(function (disk) {
-    var usedSpaceBytes = parseInt(disk['Volume Used Space'].match(/(\d+) Bytes/)[1])
-    var totalSpaceBytes = parseInt(disk['Container Total Space'].match(/(\d+) Bytes/)[1])
-    var totalFreeSpaceBytes = parseInt(disk['Container Free Space'].match(/(\d+) Bytes/)[1])
-    let totalSpaceGB = totalSpaceBytes / 1000000000    
-    var freeSpaceGB = totalFreeSpaceBytes / 1000000000
-    var usagePercentage = ((usedSpaceBytes / totalSpaceBytes) * 100).toFixed(2)
-    table.insertRecord( sanitize(disk['Device Identifier']) , 
+    var usedSpaceBytes = disk['Volume Used Space'] ? parseInt(disk['Volume Used Space'].match(/(\d+) Bytes/)[1]) : 0
+    var containerTotalSpace = disk['Container Total Space'] ? disk['Container Total Space'].match(/(\d+) Bytes/) : 0
+    var volumeTotalSpace = disk['Volume Total Space'] ? disk['Volume Total Space'].match(/(\d+) Bytes/) : 0
+    var totalSpaceBytes = containerTotalSpace ? parseInt(containerTotalSpace[1]) : (volumeTotalSpace ? parseInt(volumeTotalSpace[1]) : 0)
+    var containerFreeSpace = disk['Container Free Space'] ? disk['Container Free Space'].match(/(\d+) Bytes/) : 0
+    var volumeFreeSpace = disk['Volume Free Space'] ? disk['Volume Free Space'].match(/(\d+) Bytes/) : 0
+    var totalFreeSpaceBytes = containerFreeSpace ? parseInt(containerFreeSpace[1]) : (volumeFreeSpace ? parseInt(volumeFreeSpace[1]) : 0)
+    var usagePercentage = totalSpaceBytes > 0 ? ((usedSpaceBytes / totalSpaceBytes) * 100).toFixed(2) : 0
+    var parentDisk = disk['Part of Whole']
+  
+    if (!parentDiskValues[parentDisk]) {
+      var freeSpace = convertToAppropriateUnits(totalFreeSpaceBytes);
+      var size = convertToAppropriateUnits(totalSpaceBytes);
+      parentDiskValues[parentDisk] = {
+        freeSpace: freeSpace.value,
+        size: size.value,
+        freeSpaceUnit: freeSpace.unit,
+        sizeUnit: size.unit
+      }
+      variables.push(
+        D.createVariable(parentDisk + '-free-space', parentDisk + ' Free Space', parentDiskValues[parentDisk].freeSpace, parentDiskValues[parentDisk].freeSpaceUnit, D.valueType.NUMBER),
+        D.createVariable(parentDisk + '-size', parentDisk + ' Size', parentDiskValues[parentDisk].size, parentDiskValues[parentDisk].sizeUnit, D.valueType.NUMBER)
+      ) 
+    }
+    table.insertRecord(sanitize(disk['Device Identifier']), 
       [
         disk['Volume Name'] || 'N/A',
         disk['Device Node'] || 'N/A',
         disk['Part of Whole'] || 'N/A',
         disk['Mount Point'] || 'N/A',
         usagePercentage,
-        freeSpaceGB.toFixed(2),
-        totalSpaceGB.toFixed(2),
         disk['File System Personality'] || 'N/A',
         disk['Volume Read-Only'] || 'N/A',
         disk['SMART Status'] || 'N/A'
       ]
     )
   })
-  D.success(table)
+  D.success(variables, table)
 }
 
 /**
