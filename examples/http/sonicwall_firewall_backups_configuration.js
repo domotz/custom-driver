79ea55dc5e5910de6bedd06a2a605328575c8aae
mariemm97 <mariem.mguidich@pivasoftware.com>
2024-09-20 18:33:38 +0100

    CD-484 - Add API call for authentication

--- examples/http/sonicwall_firewall_backups_configuration.js
+++ examples/http/sonicwall_firewall_backups_configuration.js
@@ -12,6 +12,43 @@
 
 var sonicWallPort = D.getParameter('sonicWallPort')
 
+//Processes the HTTP response and handles errors
+function processResponse(d) {
+    return function process(error, response, body) {
+        if (error) {
+            console.error(error);
+            D.failure(D.errorType.GENERIC_ERROR)
+        } else if (response.statusCode == 404) {
+            D.failure(D.errorType.RESOURCE_UNAVAILABLE)
+        } else if (response.statusCode == 401) {
+            D.failure(D.errorType.AUTHENTICATION_ERROR)
+        } else if (response.statusCode != 200) {
+            D.failure(D.errorType.GENERIC_ERROR)
+        }
+        d.resolve(body)
+    };
+}
+
+/**
+ * Logs in to the SonicWALL device using basic authentication
+ * @returns A promise that resolves on successful login
+ */
+function login() {
+    var d = D.q.defer();
+    var config = {
+        url: "/api/sonicos/auth",
+        username: D.device.username(),
+        password: D.device.password(),
+        protocol: "https",
+        auth: "basic",
+        port: sonicWallPort,
+        jar: true,
+        rejectUnauthorized: false
+    }
+    D.device.http.post(config, processResponse(d))
+    return d.promise
+}
+
 /**
  * This function sends an HTTPS GET request to the SonicWall Firewall device to obtain the configuration backup
  * @returns {Promise} A promise that resolves to the configuration backup
@@ -21,23 +58,11 @@ function getConfigBackup() {
     var config = {
         url: "/api/sonicos/config/current",
         protocol: "https",
-        rejectUnauthorized: false,
         port: sonicWallPort,
-        username: D.device.username(),
-        password: D.device.password()
+        jar: true,
+        rejectUnauthorized: false,
     }
-    D.device.http.get(config, function(error, response, body){
-        if (error) {          
-            console.error(error)
-            D.failure(D.errorType.GENERIC_ERROR)
-        } else if (response.statusCode == 404) {
-            D.failure(D.errorType.RESOURCE_UNAVAILABLE)
-        } else if (response.statusCode != 200) {
-            D.failure(D.errorType.GENERIC_ERROR)
-        } else {
-            d.resolve(body)
-        }
-    })
+    D.device.http.get(config, processResponse(d))
     return d.promise
 }
 
@@ -47,7 +72,8 @@ function getConfigBackup() {
 * @documentation This procedure is used to validate if the device is correctly associated and performs the backup process
 */
 function validate(){
-    getConfigBackup()
+    login()
+        .then(getConfigBackup)
         .then(function(deviceConfiguration) {
             if (deviceConfiguration && Object.keys(deviceConfiguration).length > 0) {
                 console.log("Validation successful")
@@ -69,7 +95,8 @@ function validate(){
 * @documentation This procedure backs up the Sonicwall Firewall device configuration 
 */
 function backup(){
-    getConfigBackup()
+    login()
+        .then(getConfigBackup)
         .then(function(deviceConfiguration) {
             if (deviceConfiguration && Object.keys(deviceConfiguration).length > 0) {
                 var backup = JSON.parse(deviceConfiguration)
