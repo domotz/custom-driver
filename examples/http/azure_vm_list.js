cb97c51296c25a9b8dce92e977af6da497ab6b4e
firas <firas.aloulou@pivasoftware.com>
2024-10-08 10:36:11 +0100

    CD-486 - Add resourceGroup parameter to filter result - Adapt script - Add documentation to each method

--- examples/http/azure_vm_list.js
+++ examples/http/azure_vm_list.js
@@ -1,79 +1,124 @@
 /**
+ * Domotz Custom Driver
+ * Name: Azure VMs List
+ * Description: Monitor Azure Compute Virtual Machines: this script retrieves information about Virtual Machine image, operating system, resource group, size and location .
+ *
+ * Communication protocol is HTTPS
+ *
+ * Output:
+ * Extracts the following information from the data array:
+ *      - Name
+ *      - OS
+ *      - Location
+ *      - Size
+ *      - Image Publisher
+ *      - Image
+ *      - Image Version
+ *      - Resource Group
+ **/
+
+/**
  * @description tenantID
- * @type STRING 
+ * @type STRING
  */
-var tenantID = D.getParameter('tenantID');
+const tenantID = D.getParameter('tenantID');
 
 /**
  * @description client_id
- * @type STRING 
+ * @type STRING
  */
-var client_id = D.getParameter('client_id');
+const client_id = D.getParameter('client_id');
 
 /**
  * @description client_secret
- * @type SECRET_TEXT 
+ * @type SECRET_TEXT
  */
-var client_secret = D.getParameter('client_secret');
+const client_secret = D.getParameter('client_secret');
 
 /**
  * @description subscriptionId
- * @type STRING 
+ * @type STRING
  */
-var subscriptionId = D.getParameter('subscriptionId');
+const subscriptionId = D.getParameter('subscriptionId');
 
 /**
  * @description resourceGroup
- * @type STRING 
+ * @type STRING
  */
-var resourceGroups = D.getParameter('resourceGroups');
+const resourceGroup = D.getParameter('resourceGroup');
 
 const azureCloudLoginService = D.createExternalDevice('login.microsoftonline.com');
 const azureCloudManagementService = D.createExternalDevice('management.azure.com');
 
-var accessToken;
-var resourceGroupsToCheck = [];
+let accessToken;
 
-var vmTable = D.createTable(
-    'Azure Virtual Machines',
-    [
-      { label: 'Name', valueType: D.valueType.STRING },
-      { label: 'OS', valueType: D.valueType.STRING },
-      { label: 'Location', valueType: D.valueType.STRING },
-      { label: 'Size', valueType: D.valueType.STRING},
-      { label: 'Image Publisher', valueType: D.valueType.STRING },      
-      { label: 'Image', valueType: D.valueType.STRING },            
-      { label: 'Image Version', valueType: D.valueType.STRING },                  
-      { label: 'Resource Group', valueType: D.valueType.STRING },
-    ]
-)
+const vmTable = D.createTable('Azure Virtual Machines', [
+    {label: 'Name', valueType: D.valueType.STRING},
+    {label: 'OS', valueType: D.valueType.STRING},
+    {label: 'Location', valueType: D.valueType.STRING},
+    {label: 'Size', valueType: D.valueType.STRING},
+    {label: 'Image Publisher', valueType: D.valueType.STRING},
+    {label: 'Image', valueType: D.valueType.STRING},
+    {label: 'Image Version', valueType: D.valueType.STRING},
+    {label: 'Resource Group', valueType: D.valueType.STRING}
+])
 
+/**
+ * Checks for HTTP errors in the response and handles them by triggering appropriate failures.
+ * @param {Object} error - The error object returned from the HTTP request.
+ * @param {Object} response - The HTTP response object.
+ */
 function checkHTTPError(error, response) {
-        if (error) {          
-            console.error(error);
-            D.failure(D.errorType.GENERIC_ERROR);
-        } else if (response.statusCode == 404) 
-            D.failure(D.errorType.RESOURCE_UNAVAILABLE);
-        else if (response.statusCode == 401 || response.statusCode == 403) 
-            D.failure(D.errorType.AUTHENTICATION_ERROR);
-        else if (response.statusCode != 200)
-            D.failure(D.errorType.GENERIC_ERROR);
+    if (error) {
+        console.error(error);
+        D.failure(D.errorType.GENERIC_ERROR);
+    } else if (response.statusCode === 404) {
+        D.failure(D.errorType.RESOURCE_UNAVAILABLE);
+    } else if (response.statusCode === 401 || response.statusCode === 403) {
+        D.failure(D.errorType.AUTHENTICATION_ERROR);
+    } else if (response.statusCode !== 200) {
+        D.failure(D.errorType.GENERIC_ERROR);
+    }
 }
 
-function processLoginResponse(d) 
-{
+/**
+ * Processes the login response, extracting the access token.
+ * @param {Object} d - The deferred promise object.
+ * @returns {Function} A function to process the HTTP response.
+ */
+function processLoginResponse(d) {
     return function process(error, response, body) {
         checkHTTPError(error, response);
-
-        var bodyAsJSON = JSON.parse(body);
+        const bodyAsJSON = JSON.parse(body);
         if (bodyAsJSON.access_token) {
             accessToken = bodyAsJSON.access_token;
-            d.resolve();            
-        }
-        else {
+            d.resolve();
+        } else {
             console.error("Access token not found in response body");
             D.failure(D.errorType.AUTHENTICATION_ERROR);
         }
+    }
+}
+
+/**
+ * Processes the response from the VMs API call and populates the table with VM data.
+ * @param {Object} d - The deferred promise object.
+ * @returns {Function} A function to process the HTTP response.
+ */
+function processVMsResponse(d) {
+    return function process(error, response, body) {
+        checkHTTPError(error, response);
+        const bodyAsJSON = JSON.parse(body);
+        if (!bodyAsJSON.value) D.failure(D.errorType.GENERIC_ERROR);
+
+        let vmInfoList = bodyAsJSON.value.map(extractVmInfo);
+        if (resourceGroup.toString().toLowerCase() !== "all") {
+            vmInfoList = vmInfoList.filter(function (vm) {
+                return resourceGroup === vm.resourceGroup;
+            });
+        }
+        vmInfoList.map(populateTable);
+        d.resolve();
     }
 }
 
@@ -82,153 +127,76 @@ function processLoginResponse(d)
  * @returns A promise that resolves on successful login
  */
 function login() {
-    var d = D.q.defer();
-    var config = {
-        url: "/"+tenantID+"/oauth2/token",
-        protocol: "https",
-        headers: {
+    const d = D.q.defer();
+    const config = {
+        url: "/" + tenantID + "/oauth2/token", protocol: "https", headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-        },
-        form: {
+        }, form: {
             grant_type: "client_credentials",
             client_id: client_id,
             client_secret: client_secret,
-            resource:"https://management.azure.com\/"
-        },        
-        rejectUnauthorized: false,
-        jar: true
+            resource: "https://management.azure.com\/"
+        }, rejectUnauthorized: false, jar: true
     };
     azureCloudLoginService.http.post(config, processLoginResponse(d));
     return d.promise;
 }
 
-function processResourceGroupsResponse(d) 
-{
-    return function process(error, response, body) {
-        checkHTTPError(error, response);
+/**
+ * Sanitizes the output by removing reserved words and formatting it.
+ * @param {string} output - The string to be sanitized.
+ * @returns {string} The sanitized string.
+ */
+function sanitize(output) {
+    const recordIdReservedWords = ['\\?', '\\*', '\\%', 'table', 'column', 'history']
+    const recordIdSanitizationRegex = new RegExp(recordIdReservedWords.join('|'), 'g')
+    return output.replace(recordIdSanitizationRegex, '').slice(0, 50).replace(/\s+/g, '-').toLowerCase()
+}
 
-        var bodyAsJSON = JSON.parse(body);
-        if (!bodyAsJSON.value)
-            D.failure(D.errorType.GENERIC_ERROR);
+/**
+ * Extracts necessary information from a VM object.
+ * @param {Object} vm - The VM object containing various properties.
+ * @returns {Object|null} The extracted VM information or null if invalid.
+ */
+function extractVmInfo(vm) {
+    if (!vm || !vm.properties || !vm.properties.vmId) return null;
 
-        bodyAsJSON.value.forEach(function (resourceGroup) {
-            if (resourceGroup.name) 
-                resourceGroupsToCheck.push(resourceGroup.name);
-            });
-        d.resolve();
+    const id = sanitize(vm.properties.vmId);
+    const vmName = vm.name || "N/A";
+    const location = vm.location || "N/A";
+    const size = vm.properties.hardwareProfile && vm.properties.hardwareProfile.vmSize || "N/A";
+    const osType = vm.properties.storageProfile && vm.properties.storageProfile.osDisk && vm.properties.storageProfile.osDisk.osType || "N/A";
+    const imagePublisher = vm.properties.storageProfile && vm.properties.storageProfile.imageReference && vm.properties.storageProfile.imageReference.publisher || "N/A";
+    const imageName = vm.properties.storageProfile && vm.properties.storageProfile.imageReference && vm.properties.storageProfile.imageReference.offer || "N/A";
+    const imageVersion = vm.properties.storageProfile && vm.properties.storageProfile.imageReference && vm.properties.storageProfile.imageReference.version || "N/A";
+
+    let resourceGroup = "N/A";
+    if (vm.id) {
+        const resourceGroupMatch = vm.id.match(/\/resourceGroups\/([^\/]*)\//);
+        if (resourceGroupMatch && resourceGroupMatch[1]) resourceGroup = resourceGroupMatch[1];
+    }
+
+    return {
+        id, vmName, location, size, osType, imagePublisher, imageName, imageVersion, resourceGroup
     };
 }
 
 /**
- * Retrieve Azure resource groups
- * @returns A promise that resolves on successful request
+ * Populates a table with VM information.
+ * @param {Object} vm - The VM information to insert into the table.
  */
-function retrieveResourceGroups() {
-    var d = D.q.defer();    
-    if (resourceGroups.length === 1 && resourceGroups[0].toLowerCase() === 'all') {
-
-        var config = {
-            url: "/subscriptions/" + subscriptionId+"/resourceGroups?api-version=2021-04-01",
-            protocol: "https",
-            headers: {
-                "Authorization": "Bearer " + accessToken,
-            },
-            rejectUnauthorized: false,
-            jar: true
-        };
-        azureCloudManagementService.http.get(config, processResourceGroupsResponse(d));
-    }
-    else 
-    {
-        resourceGroupsToCheck = resourceGroups;
-        d.resolve(); 
-    }
-
-    return d.promise;
+function populateTable(vm) {
+    vmTable.insertRecord(vm.id, [vm.vmName, vm.osType, vm.location, vm.size, vm.imagePublisher, vm.imageName, vm.imageVersion, vm.resourceGroup]);
 }
-
-function sanitize (output) {
-  const recordIdReservedWords = ['\\?', '\\*', '\\%', 'table', 'column', 'history']
-  const recordIdSanitisationRegex = new RegExp(recordIdReservedWords.join('|'), 'g')
-  return output.replace(recordIdSanitisationRegex, '').slice(0, 50).replace(/\s+/g, '-').toLowerCase()
-}
-
-function extractVMinfo(vm)
-{
-    if (vm.properties && vm.properties.vmId) {
-        var id = sanitize(vm.properties.vmId);
-        var vmName = vm.name?vm.name:"N/A";
-        var location = vm.location?vm.location:"M/A";
-
-        var size = "N/A";
-        if (vm.properties.hardwareProfile && vm.properties.hardwareProfile.vmSize)
-            size = vm.properties.hardwareProfile.vmSize
-
-        var osType = "N/A";
-        var imagePublisher = "N/A";
-        var imageName = "N/A";
-        var imageVersion = "N/A";
-
-        if (vm.properties.storageProfile) {
-            if (vm.properties.storageProfile.osDisk && vm.properties.storageProfile.osDisk.osType)
-                osType = vm.properties.storageProfile.osDisk.osType;
-            
-            if (vm.properties.storageProfile.imageReference) {
-                if (vm.properties.storageProfile.imageReference.publisher)
-                    imagePublisher = vm.properties.storageProfile.imageReference.publisher;
-
-                if (vm.properties.storageProfile.imageReference.offer)
-                    imageName = vm.properties.storageProfile.imageReference.offer;
-
-                if (vm.properties.storageProfile.imageReference.version)
-                    imageVersion = vm.properties.storageProfile.imageReference.version;
-            }
-        }   
-
-        var resourceGroup = "N/A";
-        if (vm.id) {
-            const resourceGroupMatch = vm.id.match(/\/resourceGroups\/([^\/]*)\//);
-            if (resourceGroupMatch && resourceGroupMatch[1])
-                resourceGroup = resourceGroupMatch[1];
-        }
-
-        vmTable.insertRecord(id, [
-            vmName,
-            osType,
-            location,
-            size,
-            imagePublisher,
-            imageName,            
-            imageVersion,                        
-            resourceGroup
-        ]);
-    }
-}
-
-function processVMsResponse(d) 
-{
-    return function process(error, response, body) {
-        checkHTTPError(error, response);
-
-        var bodyAsJSON = JSON.parse(body);
-        if (!bodyAsJSON.value)
-            D.failure(D.errorType.GENERIC_ERROR);
-
-        bodyAsJSON.value.forEach(extractVMinfo);
-        d.resolve();
-    }
-}
-
-
 
 /**
- * Retrieve Azure VMs for a specific resource group
- * @returns A promise that resolves on successful request
+ * Retrieve Azure VMs for the configured or all resource groups
+ * @returns A promise that resolves on successful login
  */
-function retrieveVMsByResourceGroup(resourceGroup) {
-    var d = D.q.defer();
-    var config = {
-        url: "/subscriptions/" + subscriptionId+"/resourceGroups/"+resourceGroup+"/providers/Microsoft.Compute/virtualMachines?api-version=2021-04-01",
+function retrieveVMs() {
+    const d = D.q.defer();
+    const config = {
+        url: "/subscriptions/" + subscriptionId + "/providers/Microsoft.Compute/virtualMachines?api-version=2021-04-01",
         protocol: "https",
         headers: {
             "Authorization": "Bearer " + accessToken,
@@ -241,26 +209,18 @@ function retrieveVMsByResourceGroup(resourceGroup) {
 }
 
 /**
- * Retrieve Azure VMs for the configured or all resource groups
- * @returns A promise that resolves on successful login
+ * Publishes the VM table.
  */
-function retrieveVMs() {
-    const vmInfoPromises = []
-
-    resourceGroupsToCheck.forEach(function (resourceGroup) {
-        var vmInfoPromise = retrieveVMsByResourceGroup(resourceGroup);
-        vmInfoPromises.push(vmInfoPromise);
-    });
-
-    return D.q.all(vmInfoPromises)
+function publishVMTable() {
+    D.success(vmTable);
 }
 
 /**
  * @remote_procedure
- * @label Validate Azure connection 
+ * @label Validate Azure connection
  * @documentation This procedure is used to validate if data is accessible
  */
-function validate(){
+function validate() {
     login()
         .then(function () {
             D.success();
@@ -271,11 +231,6 @@ function validate(){
         });
 }
 
-function publishVMTable()
-{
-    D.success(vmTable);
-}
-
 /**
  * @remote_procedure
  * @label Get Azure VMs
@@ -283,7 +238,6 @@ function publishVMTable()
  */
 function get_status() {
     login()
-        .then(retrieveResourceGroups)
         .then(retrieveVMs)
         .then(publishVMTable)
         .catch(function (error) {
